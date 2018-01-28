2f772811f392729c454818fe614efed95b45f80c
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-01-28 09:01:00 +0600

    Add solution for problem `A - The number of positions` Link: https://codeforces.com/contest/124/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/124/submission/34622068

--- codeforces/124/A.cpp
+++ codeforces/124/A.cpp
@@ -5,6 +5,19 @@ int main()
 {
     int n,a,b,p;
     cin>>n>>a>>b;
-    cout<<n-max(a+1,n-b)+1;
-    return 0;
+    if((a+b)==n)
+    {
+        cout<<b;
+        return 0;
+    }
+    if((a+b)>n)
+    {
+        cout<<n-a;
+        return 0;
+    }
+    else
+    {
+        cout<<b+1;
+        return 0;
+    }
 }
