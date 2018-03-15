dde7c44330df1b6c05878080462ba39645d53354
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-03-15 07:45:00 +0600

    Add solution for problem `A - Nicholas and Permutation` Link: https://codeforces.com/contest/676/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/676/submission/36288889

--- codeforces/676/A.cpp
+++ codeforces/676/A.cpp
@@ -3,19 +3,33 @@ using namespace std;
 
 int main()
 {
-    int n,i,minimum,maximum;
+    int ar[1001],n,i,p=0,q=1000,a,b,m,k,array[10];
     cin>>n;
     for(i=1; i<=n; i++)
     {
-        int a;
-        cin>>a;
-        if(a==1) minimum=i;
-        if(a==n) maximum=i;
+        cin>>ar[i];
+        if(ar[i]>p)
+        {
+            p = ar[i];
+            a = i;
+        }
+        if(q>ar[i])
+        {
+            q = ar[i];
+            b = i;
+        }
     }
-    int x;
-    x = max(minimum, maximum);
-    int y;
-    y = min(minimum, maximum);
-    cout<<max((n-y), (x-1));
+   // cout<<a<<" "<<b<<endl;
+   for(int j=0; j<10; j++)
+   {
+       array[j] = 0;
+   }
+    array[0] = abs(a-b);
+    array[1] = abs(n-a);
+    array[3] = abs(n-b);
+    array[4] = abs(a-1);
+    array[5] = abs(b-1);
+    sort(array, array+6);
+    cout<<array[5];
     return 0;
 }
