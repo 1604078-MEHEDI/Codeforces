3f54173621b7426f77b7c7349d5946ed704e1f1c
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-03-26 10:00:00 +0600

    Add solution for problem `A - Tritonic Iridescence` Link: https://codeforces.com/contest/957/problem/A Tags: implementation, *1300 Ref: https://codeforces.com/contest/957/submission/36631368

--- codeforces/957/A.cpp
+++ codeforces/957/A.cpp
@@ -3,41 +3,70 @@ using namespace std;
 
 int main()
 {
-    int n,i;
+    int n,count=0,ar[101],j=0,p,a=0,q,t,m;
     cin>>n;
     string s;
     cin>>s;
 
-    for(i=0; i<n-1; i++)
+    for(int i=0; i<n; i++)
     {
-        if(s[i] != '?' && s[i]==s[i+1])
+        if(s[i]=='?')
+        {
+            count++;
+            ar[j++] = i;
+        }
+        else if(s[i]==s[i+1])
         {
             cout<<"No";
             return 0;
         }
     }
 
-    for(i=0; i<n; i++)
+    if(count>0)
     {
-        if(s[i] == '?')
-        {
-            if(i == 0 || i==n-1)
+        //if(count==1)
+       // {
+            p = ar[0];
+
+            if(p==0 || p==n-1)
             {
                 cout<<"Yes";
                 return 0;
             }
-            if(s[i+1] == '?')
+            if(count==1)
             {
-                cout<<"Yes";
+            if(s[p-1]!=s[p+1])
+            {
+                cout<<"No";
                 return 0;
-            }
-            if(s[i-1] == s[i+1])
-            {
-                cout<<"Yes";
-                 return 0;
             }
         }
+     /*   t = ar[0];
+        q = ar[j-1];
+        if(t==0 || q==n-1)
+        {
+            cout<<"Yes";
+            return 0;
+        }*/
+
+        for(int k=0; k<j; k++)
+        {
+            int m=0;
+            p = ar[k];
+            if(s[p-1]==s[p+1] || s[p]==s[p+1])
+            {
+                cout<<"Yes";
+                return 0;
+            }
+        }
+        if(m==0)
+        {
+            cout<<"No";
+            return 0;
+        }
     }
+else
+{
     cout<<"No";
-    return 0;
+}
 }
