748e738b7508099de638dc90eddd7ab6146da56f
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-04-09 21:53:00 +0600

    Add solution for problem `A - Cheap Travel` Link: https://codeforces.com/contest/466/problem/A Tags: implementation, *1200 Ref: https://codeforces.com/contest/466/submission/37126226

--- codeforces/466/A.cpp
+++ codeforces/466/A.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n,m,a,b,count=0,ar[10],s,p;
-    for(int i=0; i<10; i++)ar[i] = 9999999;
+    int n,m,a,b,count=0,ar[10];
+    long long x=9999999,y=9999999,z=9999999,w=9999999,s=9999999,p=9999999,t=9999999;
     cin>>n>>m>>a>>b;
 
-    ar[0] = n*a;
-    if(n%m==0)ar[1] = (n/m)*b;
-    else ar[2] = ((n/m)+1)*b;
-    if(m>=n)ar[3] = b;
+    x = n*a;
+    if(n%m==0)y = (n/m)*b;
+    else w = ((n/m)+1)*b;
+    if(m>=n)z = b;
     //cout<<z<<endl;
 
     while(n>0)
@@ -23,11 +23,17 @@ int main()
         {
             p = (n/m)*b;
 
-            ar[4] = s + p;
+            t = s + p;
             break;
         }
     }
+    ar[0] = x;
+    ar[1] = y;
+    ar[2] = z;
+    ar[3] = w;
+    ar[4] = t;
+
     sort(ar, ar+5);
-//for(int i=0; i<5; i++)
+
     cout<<ar[0];
 }
