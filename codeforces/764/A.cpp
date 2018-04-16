8ea8550c41e13395edc98522952c687d295ec2af
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-04-16 20:23:00 +0600

    Add solution for problem `A - Taymyr is calling you` Link: https://codeforces.com/contest/764/problem/A Tags: brute force, implementation, math, *800 Ref: https://codeforces.com/contest/764/submission/37375297

--- codeforces/764/A.cpp
+++ codeforces/764/A.cpp
@@ -1,21 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int ar[1000000];
+
 int main()
 {
     ios_base::sync_with_stdio(false);
 
-    int n,m,z;
-    int i,count=0;
-
+    int n,m,z,p,q,count=0;
     cin>>n>>m>>z;
 
-    for(i=0; i<=z; i +=n)
+
+    for(int i=1; i<=z; i++)
     {
-        if(i % m == 0)count++;
+        p = i*n;
+        if(p > z)break;
+        ar[p]++;
+    }
+    for(int i=1; i<=z; i++)
+    {
+        q = i*m;
+        if(q > z)break;
+        ar[q]++;
     }
 
-    cout<<count-1;
-    return 0;
+    for(int i=1; i<=z; i++)
+    {
+        if(ar[i] > 1)count++;
+    }
 
+    cout<<count<<endl;
+    return 0;
 }
