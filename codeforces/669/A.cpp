90dc42e4ce2608665d2b6cd5cbd72deb22038d0d
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-04-17 06:25:00 +0600

    Add solution for problem `A - Little Artem and Presents` Link: https://codeforces.com/contest/669/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/669/submission/37384505

--- codeforces/669/A.cpp
+++ codeforces/669/A.cpp
@@ -1,22 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void solve(long long int n)
-{
-    long long answer;
-    if(n%3==0)answer = 2*n/3;
-    else
-        answer = 2*(n-n%3)/3 + 1;
-    cout<<answer<<endl;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(0);
 
-    long long n;
+    int n,i=1, count=0;
     cin>>n;
-    solve(n);
+
+    while(n >= 0)
+    {
+        if(n <= 0)break;
+        count++;
+        if(i % 2 == 0)n--;
+        else n -= 2;
+        i++;
+    }
+    cout<<count;
     return 0;
 }
