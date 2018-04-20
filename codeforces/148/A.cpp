d07f34a683509084a2af882bfdf5ce8989c65891
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-04-20 15:40:00 +0600

    Add solution for problem `A - Insomnia cure` Link: https://codeforces.com/contest/148/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/148/submission/37482288

--- codeforces/148/A.cpp
+++ codeforces/148/A.cpp
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
 int main()
 {
-    int k,l,m,n,i,d,count=0;
-    scanf("%d %d %d %d %d",&k,&l,&m,&n,&d);
-    for(i=1; i<=d; i++)
+    //ios_base::sync_with_stdio(false);
+  //  cin.tie(0);
+
+    int k,l,m,n,count=0,d;
+   // cin>>k>>l>>m>>n>>d;
+   scanf(" %d %d %d %d %d", &k, &l, &m, &n, &d);
+    for(int i=1; i <= d; i++)
     {
-        if(i%k == 0 || i%l == 0 || i%m == 0 || i%n == 0)
+        if( i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0)
             count++;
     }
-    printf("%d",count);
+
+   // cout<<count<<endl;
+   printf("%d", count);
     return 0;
 }
