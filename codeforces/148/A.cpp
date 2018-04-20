94d41d4a406dffe234409209e2b8ad2cc3ecd5bd
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-04-20 15:28:00 +0600

    Add solution for problem `A - Insomnia cure` Link: https://codeforces.com/contest/148/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/148/submission/37482008

--- codeforces/148/A.cpp
+++ codeforces/148/A.cpp
@@ -1,20 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int ar[100001];
+
+void afk(int k,int d)
+{
+    int i=0,p=0;
+    while(p <= d)
+    {
+        i++;
+        p = i*k;
+        ar[p] = 1;
+    }
+}
+void afl(int l, int d)
+{
+    int i=0,p=0;
+    while(p <= d)
+    {
+        i++;
+        p = i*l;
+        ar[p] = 1;
+    }
+}
+void afm(int m, int d)
+{
+    int i=0,p=0;
+    while(p <= d)
+    {
+        i++;
+        p = i*m;
+        ar[p] = 1;
+    }
+}
+void afn(int n, int d)
+{
+    int i=0,p=0;
+    while(p <= d)
+    {
+        i++;
+        p = i*n;
+        ar[p] = 1;
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int k,l,m,n,count=0,d;
+    int k,l,m,n,d,count=0;
     cin>>k>>l>>m>>n>>d;
-
-    for(int i=1; i <= d; i++)
+    afk(k,d);
+    afl(l,d);
+    afm(m,d);
+    afn(n,d);
+    for(int i=1; i<=d; i++)
     {
-        if( i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0)
-            count++;
+        if(ar[i]==0)count++;
     }
 
-    cout<<count<<endl;
-    return 0;
+    cout<<d-count<<endl;
+
 }
