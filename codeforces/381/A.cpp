d19d5d68ac1248ad6695e68ed89d2d5e3ebdc224
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-05-09 17:12:00 +0600

    Add solution for problem `A - Sereja and Dima` Link: https://codeforces.com/contest/381/problem/A Tags: greedy, implementation, two pointers, *800 Ref: https://codeforces.com/contest/381/submission/38068804

--- codeforces/381/A.cpp
+++ codeforces/381/A.cpp
@@ -6,32 +6,42 @@ int ar[10001];
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    int n,i;
+    int n,sum = 0,i;
     int sareja = 0,dima = 0;
 
     cin >> n;
+
     for( int j = 0; j < n; j++)
     {
         cin >> ar[j];
+        sum += ar[j];
     }
     int cnt =0;
     i = 0;
     int k = n - 1;
-    int ss = 0,dd = 0,mx;
-    while(i <= k)
+    int ss = 0,dd = 0;
+    while(1)
     {
-        cnt++;
-        if(ar[i] > ar[k]) mx = ar[i++];
-        else   mx = ar[k--];
-        if(cnt % 2 == 1)
+        int mx = max(ar[i], ar[k]);
+
+        if(ss == 0)
         {
+            if(mx == ar[i])i++;
+            if(mx == ar[k])k--;
             sareja += mx;
+            ss++;
+            dd = 0;
         }
-        else
+        else if(dd == 0)
+        {
+            if(mx == ar[i])i++;
+            if(mx == ar[k])k--;
             dima += mx;
+            ss = 0;
+            dd++;
+        }
+        cnt++;
+        if( cnt >= n)break;
     }
     cout<<sareja <<" "<<dima<<endl;
     return 0;
