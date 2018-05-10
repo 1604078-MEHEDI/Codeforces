21f40e7d1960b99f34dde4c7464d90e33f5146ae
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-05-10 16:14:00 +0600

    Add solution for problem `A - Night at the Museum` Link: https://codeforces.com/contest/731/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/731/submission/38088475

--- codeforces/731/A.cpp
+++ codeforces/731/A.cpp
@@ -1,18 +1,33 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
+    map <char, int> mp;
+    char c = 'a', ck = 'a';
+    for(int i = 1; i <= 26; i++)
+    {
+       // cout<<c<<" ";
+        mp[c] = i;
+        c++;
+    }
     string s;
     cin >> s;
-    char ck = 'a' ;
     int mv = 0,total_move = 0;
     for(int i = 0; i < s.length(); i++)
     {
-        mv = abs(s[i] - ck);
-        total_move += min(mv, 26-mv);
-        ck = s[i];
+        if ( mp[ck] > mp[s[i]])
+        {
+            mv = min( (mp[ck] - mp[s[i]] ), (mp[s[i]] - mp[ck] + 26) );
+            ck = s[i];
+            total_move += mv;
+        }
+        else
+        {
+             mv = min( (mp[s[i]] - mp[ck] ), (mp[ck] - mp[s[i]] + 26) );
+             ck = s[i];
+             total_move += mv;
+        }
     }
 
     cout<<total_move<<"\n";
