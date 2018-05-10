04639e5fb1f43f48689107a8c7ffc76f1e8eba39
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-05-10 15:33:00 +0600

    Add solution for problem `A - Black Square` Link: https://codeforces.com/contest/431/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/431/submission/38087780

--- codeforces/431/A.cpp
+++ codeforces/431/A.cpp
@@ -3,24 +3,22 @@ using namespace std;
 
 int main()
 {
-    //ios_base::sync_with_stdio(0);
-   // cin.tie(0);
-    //cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int a[5],sum = 0;
     for(int i = 1; i <= 4; i++)
-        scanf("%d", &a[i]);
+        cin >> a[i];
 
     string str;
 
     cin >> str;
-   // scanf("%s", str);
 
     for(int i = 0; i < str.length(); i++)
     {
         sum += a[str[i]- '0' ] ;
     }
 
-//    cout<<sum<<"\n";
-printf("%d\n", sum);
+    cout<<sum<<"\n";
     return 0;
 }
