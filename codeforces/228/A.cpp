66778823269584c13d9130d6d13a15f76b4e1f8b
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-05-11 07:46:00 +0600

    Add solution for problem `A - Is your horseshoe on the other hoof?` Link: https://codeforces.com/contest/228/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/228/submission/38103604

--- codeforces/228/A.cpp
+++ codeforces/228/A.cpp
@@ -4,8 +4,7 @@ using namespace std;
 set < int > s;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0) ;   //int ck = 1,ans = 0;
+    //int ck = 1,ans = 0;
     for(int i = 0; i < 4; i++)
     {
         int a;
