ec00d112045f2145a3f8754871ebeb05ba84bc9e
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-05-11 07:51:00 +0600

    Add solution for problem `A - Is your horseshoe on the other hoof?` Link: https://codeforces.com/contest/228/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/228/submission/38103642

--- codeforces/228/A.cpp
+++ codeforces/228/A.cpp
@@ -1,14 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main () {
-    int n, k;
-    set < int > s;
-    for(int i = 0; i<4; i++)
+//map < int , int> mp;
+set < int > s;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0) ;
+   // cout.tie(0);
+      //int ck = 1,ans = 0;
+    for(int i = 0; i < 4; i++)
     {
-        cin >> n;
-        s.insert(n);
+        int a;
+        cin >> a;
+        //mp[a]++;
+        s.insert(a);
     }
-    cout << 4-s.size();
-    return 0;
+    cout<<4 - s.size();
+    //cout<<ans<<endl;
 }
-
