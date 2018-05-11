5a1b6859e92d7ccf366a0d804ebf3dda151e3f1f
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-05-12 02:29:00 +0600

    Add solution for problem `A - Die Roll` Link: https://codeforces.com/contest/9/problem/A Tags: math, probabilities, *800 Ref: https://codeforces.com/contest/9/submission/38123247

--- codeforces/9/A.cpp
+++ codeforces/9/A.cpp
@@ -7,9 +7,15 @@ int main()
 
     cin >> y >> w;
 
-    int ans = 7 - max(y, w);
-
-    cout<<ans/__gcd(6, ans)<<"/"<<6/__gcd(6, ans)<<"\n";
-
+    int mx = max(y, w);
+    int ans = 7 - mx;
+    if(ans == 7)cout<<0<<"/"<<1<<"\n";
+    else if(ans == 6) cout<<1<<"/"<<1<<"\n";
+    else if(ans == 5)cout<<5<<"/"<<6<<"\n";
+    else if(ans == 4)cout<<2<<"/"<<3<<"\n";
+    else if(ans  == 3)cout<<1<<"/"<<2<<"\n";
+    else if(ans == 2)cout<<1<<"/"<<3<<"\n";
+    else if(ans == 1)cout<<1<<"/"<<6<<"\n";
+    else cout<<0<<"/"<<1<<"\n";
     return 0;
 }
