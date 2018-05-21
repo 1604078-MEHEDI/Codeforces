d0a9495d60269d451f6731bd8e807e4008b4caa6
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-05-21 22:48:00 +0600

    Add solution for problem `A - Chess Placing` Link: https://codeforces.com/contest/985/problem/A Tags: implementation, *1100 Ref: https://codeforces.com/contest/985/submission/38511903

--- codeforces/985/A.cpp
+++ codeforces/985/A.cpp
@@ -1,66 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define IO ios_base::sync_with_stdio(0),cin.tie(0);
-
-int ar[1000],br[1000],cr[1000];
+int ar[1000],br[1000];
 
 int main()
 {
-    IO
     int n;
-
-    cin >> n;
-    int cnt = 0,count=0;
-    for(int i = 1; i <= n/2; i++)
+    cin >>n;
+    for(int i =1; i <= n/2; i++)
     {
-        cin >>ar[i];
-        if(ar[i] %2 == 0)cnt++;
-        br[ar[i]] = 999;
-        cr[ar[i]] = 999;
+        int x;
+        cin >>x;
+        ar[i] = x;
     }
     sort(ar+1, ar+n/2+1);
-    if(cnt == n/2)
+    int odd=0;
+   int i,x=1;
+    for(i=1;i<=n;i=i+2)
     {
-        cout<<0<<endl;
-        return 0;
+        odd=odd+abs(ar[x++]-i);
     }
-    int mv1 = 0, mv2 = 0,kk=2,k=1;
-        for(int i = 1; i <= n/2; i++)
-        {
-            if(ar[i] % 2 != 0)
-            {
-                for(int j = kk; j <= n; j += 2 )
-                {
-                    if(br[j] == 0)
-                    {
-                        mv1 += abs(j - ar[i]);
-                        br[j] = 999;
-                        kk = j+2;
-                        break;
-                    }
-                }
-            }
-        }
-       // cout<<count<<endl;
-        for(int i = 1; i <= n/2; i++)
-        {
-            if(ar[i] % 2 == 0)
-            {
-                for(int j = k; j <= n; j += 2 )
-                {
-                    if(cr[j] == 0)
-                    {
-                       //cout<<j<<"--"<<ar[i]<<endl;
-                        mv2 += abs(j - ar[i]);
-                        cr[j] == 999;
-                        k = j+2;
-                        break;
-                    }
-                }
-            }
-        }
-
-    cout<<min(mv1,mv2)<<endl;
-    return 0;
+    int even=0;
+    x=1;
+    for(i=2;i<=n;i=i+2)
+    {
+        even=even+abs(ar[x++]-i);
+    }
+    cout<<min(odd,even)<<endl;
 }
