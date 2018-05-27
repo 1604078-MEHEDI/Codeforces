b5937f74bc124718e3b0a517de3149387cb95681
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-05-27 21:12:00 +0600

    Add solution for problem `A - Antipalindrome` Link: https://codeforces.com/contest/981/problem/A Tags: brute force, implementation, strings, *900 Ref: https://codeforces.com/contest/981/submission/38662718

--- codeforces/981/A.cpp
+++ codeforces/981/A.cpp
@@ -1,23 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define IO ios_base::sync_with_stdio(false),cin.tie(0);
+#define IO ios_base::sync_with_stdio(0),cin.tie(0);
 #define ll long long
+bool isPalRec(string str,int s, int e)
+{
+    if (s == e)
+    return true;
 
+
+    if (str[s] != str[e])
+    return false;
+
+    if (s < e + 1)
+    return isPalRec(str, s + 1, e - 1);
+
+    return true;
+}
+
+bool isPalindrome(string str)
+{
+int n = str.length();
+if (n == 0)
+    return true;
+
+return isPalRec(str, 0, n - 1);
+}
+map<char,int>m;
 int main()
 {
     IO
     string s;
-    cin >> s;
+    cin>>s;
 
-    string t = s;
-    reverse(t.begin(), t.end());
-
-    if(s != t)
-        cout<<s.length()<<"\n";
-    else if(s == string(s.length(), s[0]))
-        cout<<"0\n";
+    for(int i = 0; i < s.length(); i++)
+    {
+        m[s[i]]++;
+    }
+    if(m.size() == 1)
+    {
+        cout<<0<<endl;
+        return 0;
+    }
+    if(isPalindrome(s))
+        cout<<s.length()-1;
     else
-        cout<<s.length() - 1<<"\n";
-    return 0;
+        cout<<s.length();
 }
