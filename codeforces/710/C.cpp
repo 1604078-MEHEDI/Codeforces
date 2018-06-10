356defdf38c16dac7c0a49cd2a22aa5ec6e9384c
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-06-10 10:51:00 +0600

    Add solution for problem `C - Magic Odd Square` Link: https://codeforces.com/contest/710/problem/C Tags: constructive algorithms, math, *1500 Ref: https://codeforces.com/contest/710/submission/39083275

--- codeforces/710/C.cpp
+++ codeforces/710/C.cpp
@@ -1,29 +1,28 @@
+///Help from -> Matrix.code
 #include<bits/stdc++.h>
 using namespace std;
 #define IO ios_base::sync_with_stdio(false),cin.tie(0);
 #define ll long long
 
+int f(int n,int x, int y)
+{
+    int m = (x + 2 * y + 1)%n;
+   // cout<<"("<<m<<")";
+    return m;
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    int even = 2, odd = 1;
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0;  j < n; j++)
-        {
-            if(min(i, n - i - 1) + min(j, n - j - 1) >= n/2)
-            {
-                cout<<odd<<" ";
-                odd += 2;
-            }
-            else
-            {
-                cout<<even<<" ";
-                even += 2;
-            }
-        }
-        cout<<endl;
-    }
+  IO
+  int n;
+  cin >> n;
+  for(int i = 0; i < n; i++)
+  {
+      for(int j = 0; j < n; j++)
+      {
+          cout<<f(n, n - j - 1, i)*n + f(n, j, i) + 1<<" ";
+      }
+      cout<<endl;
+  }
+  return 0;
 }
