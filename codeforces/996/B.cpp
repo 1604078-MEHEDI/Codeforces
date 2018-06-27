2eeba87f9bbc2d5d8825dfeb8f17a538e2cc2f10
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-06-27 13:31:00 +0600

    Add solution for problem `B - World Cup` Link: https://codeforces.com/contest/996/problem/B Tags: binary search, math, *1300 Ref: https://codeforces.com/contest/996/submission/39694227

--- codeforces/996/B.cpp
+++ codeforces/996/B.cpp
@@ -1,29 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define inf 10000000001
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL),cout.tie(NULL);
-   int n;
-	cin>>n;
-	int array[n];
-	for(int i=0;i<n;i++)
-    {
-		cin>>array[i];
-	}
-	//int count=0;
-	while(1)
-    {
-		for(int i=0;i<n;i++)
-		{
-			if(array[i]<=i)
-			{
-				cout<<i+1;
-				return 0;
-			}
-			array[i]=array[i]-n;
-		}
-	}
+
+    int n;
+    cin >>n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    while(1){
+        for(int i = 0; i < n; i++){
+            if(i + 1 > a[i]){
+                cout<<i+1<<endl;
+                return 0;
+            }
+            a[i] = a[i] - n;
+        }
+    }
+    return 0;
 }
