7785f52b086482eef6b7b7f54d3912e6ee26316b
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-06-29 12:55:00 +0600

    Add solution for problem `B - Easter Eggs` Link: https://codeforces.com/contest/78/problem/B Tags: constructive algorithms, implementation, *1200 Ref: https://codeforces.com/contest/78/submission/39760777

--- codeforces/78/B.cpp
+++ codeforces/78/B.cpp
@@ -8,23 +8,23 @@ int main()
 
     int n;
     cin >> n;
-   // string s = "ROYGBIV";
+    string s = "ROYGBIV";
     string str = "ROYGBIV";
     for(int i = 7; i < n; i++){
-//            int j = 0;
-//            if(n - i <= 3){
-//                for(int k = 0; k <= 3-(n - i); k++){
-//                    if(str[k] == s[j])j++;
-//                }
+            int j = 0;
+            if(n - i <= 3){
+                for(int k = 0; k <= 3-(n - i); k++){
+                    if(str[k] == s[j])j++;
+                }
 
-            //}
-//            while(1){
-//                if(str[i-1] == s[j] || str[i-2] == s[j] || str[i-3] == s[j]){
-//                    j++;
-//                }
-//                else break;
-//            }
-    str +=str[i - 4];
+            }
+            while(1){
+                if(str[i-1] == s[j] || str[i-2] == s[j] || str[i-3] == s[j]){
+                    j++;
+                }
+                else break;
+            }
+    str +=s[j];
     }
     cout<<str<<endl;
     return 0;
