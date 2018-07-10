5570638a013580fcffcffad3bddf6e25dd5998ce
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-07-10 22:17:00 +0600

    Add solution for problem `D - Polycarp and Div 3` Link: https://codeforces.com/contest/1005/problem/D Tags: dp, greedy, number theory, *1500 Ref: https://codeforces.com/contest/1005/submission/40177678

--- codeforces/1005/D.cpp
+++ codeforces/1005/D.cpp
@@ -4,44 +4,41 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
+    cin.tie(NULL),cout.tie(NULL);
+    int cnt = 0, sum = 0, s1 = 0;
     string s;
-    cin >> s;
-    int sum=0,cnt=0,s1 = 0,ck=0;
+    cin>>s;
+
     for(int i = 0; i < s.length(); i++)
     {
-        int p = (s[i] - '0');
-        sum += s[i] - '0';
-        if(i < s.length()- 1)
-        s1 = (s[i] - '0')+(s[i + 1] - '0');
-
-        if(p % 3 == 0)
+        int k = s[i] - '0';
+        sum+=s[i]-'0';
+        s1 = (s[i]-'0') + (s[i+1]-'0');
+        if(k%3==0)
         {
-           // s[i] = '0';
+            s[i] = '0';
             cnt++;
             sum = 0;
             s1 = 0;
         }
-        else if(sum % 3 == 0 && sum > 0)
+        else if(sum%3==0 && sum!=0)
         {
-           // s[i] = '0';
-          // cout<<sum<<endl;
+            s[i] = '0';
             cnt++;
             sum = 0;
             s1 = 0;
         }
-        else if(s1 % 3 == 0 && s1 >0)
+        else if(s1%3==0 && s1!=0)
         {
-          //  s[i] = '0';
+            s[i] = '0';
             cnt++;
-           // cout<<s1<<endl;
             i++;
             sum = 0;
             s1 = 0;
         }
-       // cout<<p<<" "<<sum<<" "<<s1<<" "<<cnt<<endl;
     }
+
     cout<<cnt<<endl;
+
     return 0;
 }
