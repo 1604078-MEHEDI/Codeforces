692289bfe6c65fb741bc67dd6e3730f3903c4f41
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-07-13 21:51:00 +0600

    Add solution for problem `C - Reorder the Array` Link: https://codeforces.com/contest/1008/problem/C Tags: combinatorics, math, *1300 Ref: https://codeforces.com/contest/1008/submission/40285095

--- codeforces/1008/C.cpp
+++ codeforces/1008/C.cpp
@@ -9,21 +9,16 @@ int main()
     cin.tie(NULL),cout.tie(NULL);
     int n;
     cin >> n;
-    int  mn = 1e9+1;
     for(int i = 0; i < n; i++){
         cin >> a[i];
-        mn = min(mn, a[i]);
     }
     sort(a, a+n);
     for(int i = 0; i < n; i++){
-            if(a[i] == mn)continue;
         q.push(a[i]);
     }
     int cnt = 0;
     for(int i = 0; i < n; i++){
-        int test = 0;
         while(!q.empty()){
-            test++;
             if(q.front() > a[i]){
                 cnt++;
                 q.pop();
@@ -33,7 +28,6 @@ int main()
                 q.pop();
             }
         }
-    if(test == 0)break;
     }
     cout<<cnt<<endl;
     return 0;
