25c04c46152a548814280c02be9a79b389d7942c
Mehedi Hasan <hasanmahadi877@gmail.com>
2018-12-10 12:44:00 +0600

    Add solution for problem `M - The Pleasant Walk` Link: https://codeforces.com/contest/1090/problem/M Tags: implementation, *1000 Ref: https://codeforces.com/contest/1090/submission/46839939

---
+++ codeforces/1090/M.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int n,k;
+    cin >> n >> k;
+    int cnt = 1;
+    int a[n+2];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int ans = 0;
+    for(int i = 0; i < n - 1; i++){
+        if(a[i] != a[i+1])cnt++;
+        else{
+            ans = max(cnt, ans);
+            cnt = 1;
+        }
+    }
+    cout << max(cnt, ans)<<endl;
+    return 0;
+}
