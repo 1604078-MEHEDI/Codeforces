3bed576f0cf1b1ff5ba2c23612e453fa4f9a62d3
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-01-26 21:54:00 +0600

    Add solution for problem `B - Digital root` Link: https://codeforces.com/contest/1107/problem/B Tags: math, number theory, *1000 Ref: https://codeforces.com/contest/1107/submission/49001513

---
+++ codeforces/1107/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1<<30
+#define MAX 10005
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+
+
+int main()
+{
+    FASTIO
+    ///*
+    //double start_time = clock();
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+    //*/
+
+    int q;
+    cin >> q;
+    while(q--){
+        ll k,x;
+        cin >> k >> x;
+        cout << (k-1)*9 + x<<endl;
+    }
+    
+
+    //double end_time = clock();
+    //printf( "Time = %lf ms\n", ( (end_time - start_time) / CLOCKS_PER_SEC)*1000);
+
+    return 0;
+}
