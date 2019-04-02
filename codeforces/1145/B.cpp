aa56ae090ead17b55183dcc8ba2d438ae917db25
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-04-02 11:05:00 +0600

    Add solution for problem `B - Kanban Numbers` Link: https://codeforces.com/contest/1145/problem/B Tags: brute force Ref: https://codeforces.com/contest/1145/submission/52218815

--- codeforces/1145/B.cpp
+++ codeforces/1145/B.cpp
@@ -1,13 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
+//void rev(int n_mb)
+//{
+//    const int N = 1024 * 1000 * n_mb;
+//    char *tab = new char[N];
+//    for(int i = 0; i < N; i++){
+//        tab[i] = i;
+//    }
+//}
+
 int main()
 {
     set<int> yes ={2, 3, 4, 5, 12, 30, 35, 43, 46, 52, 64, 86};
-//    set<int> no = {1, 10, 11, 13, 24};
+    set<int> no = {1, 10, 11, 13, 24};
     int n;
     cin >> n;
     if(yes.count(n))cout << "YES\n";
+//    else if(no.count(n)){
+//        cout << "NO\n";
+//    }
     else{
+       // rev(n);
         cout << "NO\n";
     }
 }
