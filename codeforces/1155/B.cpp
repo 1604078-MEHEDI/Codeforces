f4d97143da3854e0a7a1a32d9c018ddbf2cdb6d5
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-04-22 21:44:00 +0600

    Add solution for problem `B - Game with Telephone Numbers` Link: https://codeforces.com/contest/1155/problem/B Tags: games, greedy, implementation, *1200 Ref: https://codeforces.com/contest/1155/submission/53149510

---
+++ codeforces/1155/B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1<<30
+#define endl '\n'
+#define maxn 100005
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+const int dx4[] = { -1, 0, 1, 0 }; // 4 direction i
+const int dy4[] = { 0, 1, 0, -1 }; // 4 direction j
+const int dx8[] = { -1, -1, -1, 0, 0, 1, 1, 1 }; // 8 direction i
+const int dy8[] = { -1, 0, 1, -1, 1, -1, 0, 1 }; // 8 direction j
+const int dxk[] = { -2, -2, -1, -1, 1, 1, 2, 2 }; // knight direction i
+const int dyk[] = { -1, 1, -2, 2, -2, 2, -1, 1 }; // knight direction j
+#define dbg(x) cerr << #x << " = " << x << endl;
+#define dbg2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl;
+#define dbg3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+
+
+int main()
+{
+    FASTIO
+    ///*
+    //double start_time = clock();
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif    //*/
+
+    int n;
+    string s;
+    cin >> n >> s;
+    string tmp = s;
+    int p = (n - 11)/2;
+    int cnt = 0;
+    //int last;
+    //cerr << n - 10<<endl;
+    for(int i = 0; i < n - 10; i++){
+        //cerr << "enter--\n";
+        if(s[i] == '8'){
+            //last = i;
+            cnt++;
+        }
+    }
+    if(cnt <= p){
+        cout << "NO\n";
+        //return 0;
+    }
+    else {
+        //cerr << n-11<<endl;
+        cout <<  "YES\n";
+    }
+    //double end_time = clock();
+    //printf( "Time = %lf ms\n", ( (end_time - start_time) / CLOCKS_PER_SEC)*1000);
+    return 0;
+}
