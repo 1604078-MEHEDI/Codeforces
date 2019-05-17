c9aeef7e436d8fb0181853d08988dda8ed6de59b
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-05-17 12:48:00 +0600

    Add solution for problem `G - Petya's Exams` Link: https://codeforces.com/contest/978/problem/G Tags: greedy, implementation, sortings, *1700 Ref: https://codeforces.com/contest/978/submission/54267337

--- codeforces/978/G.cpp
+++ codeforces/978/G.cpp
@@ -1,45 +1,138 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct ank{
-    int s,d,c,id;
-}a[101];
+int res[101];
 
-int ans[101];
-bool cmp(const ank &a, const ank &b)
+struct ank
 {
-    return a.d < b.d;
-}
+    int ques, exam, prep, ID;
+    ank(int ques, int exam, int prep,int ID)
+        : ques(ques), exam(exam), prep(prep), ID(ID)
+    {
+    }
+};
+
+struct cmp
+{
+    bool operator()(ank const& a, ank const& b)
+    {
+        if(a.ques == b.ques) return a.exam > b.exam;
+        return a.ques > b.ques;
+    }
+};
+struct cmp2
+{
+    bool operator()(ank const& a, ank const& b)
+    {
+        return a.exam > b.exam;
+    }
+};
+
+priority_queue<ank, vector<ank>, cmp>PQ;
+priority_queue<ank, vector<ank>, cmp2>ans;
 
 int main()
 {
+   // freopen("in.txt", "r", stdin);
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-
-    int n,m;
+    int n, m;
     cin >> n >> m;
-    for(int i = 1; i <= m; i++){
-        cin >> a[i].s >> a[i].d >> a[i].c;
-        ans[a[i].d] = m+1;
-        a[i].id = i;
+    int day = 1;
+    vector<int>Exam;
+    map<int,bool> mp, ck;
+    for(int i = 1; i <= m; i++)
+    {
+        int s,d,c;
+        cin >> s >> d >> c;
+        Exam.push_back(d);
+        mp[d] = true;
+        PQ.push(ank(s,d,c,i));
     }
-    sort(a, a+m+1, cmp);
-
-    for(int i = 1; i <= m; i++){
-        for(int k = a[i].s; k < a[i].d && a[i].c; k++){
-            if(ans[k] == 0){
-                ans[k] = a[i].id;
-                a[i].c--;
+    while(!PQ.empty())
+    {
+        auto x = PQ.top();
+        int s = x.ques;
+        int d = x.exam;
+        int c = x.prep;
+        int yd = x.ID;
+        //cout << x.ques << " "<<x.exam << " "<<x.prep << " "<<x.ID<<endl;
+        PQ.pop();
+        while(!ans.empty())
+        {
+            int flag = 0;
+            auto y = ans.top();
+            int p = y.ques;
+            int q = y.exam;
+            int r = y.prep;
+            int id = y.ID;
+            ans.pop();
+            day = min(day, p);
+            //cout << y.ques << " **  "<<y.exam << " **  "<<y.prep << " ** "<<y.ID<<endl;
+           // cout << day << "~~~~~~~~~~~~~"<<s<<endl;
+            while( day < s && r > 0)
+            {
+                if(day >= q)
+                {
+                    flag = 1;
+                    break;
+                }
+                if(p > day || mp[day] == true || res[day] > 0)
+                {
+                    day++;
+                    continue;
+                }
+                res[day]  = id;
+               // cout << res[day] << "------"<<day << " ----- "<<r <<  endl;
+                r--;
+                day++;
+                flag = 1;
             }
+            if(r > 0) ans.push(ank(p,q,r,id));
+            if(flag) break;
+            if(day >= s || r <= 0) break;
         }
-        if(a[i].c){
+        ans.push(ank(s,d,c,yd));
+    }
+
+    while(!ans.empty())
+    {
+        int flag = 0;
+        auto y = ans.top();
+        int p = y.ques;
+        int q = y.exam;
+        int r = y.prep;
+        int id = y.ID;
+        ans.pop();
+        day = min(day, p);
+        while( day < q && r > 0)
+        {
+            if(day >= q) break;
+            if(p > day || mp[day] == true || res[day] > 0)
+            {
+                day++;
+                continue;
+            }
+            res[day]  = id;
+            r--;
+            // cout << res[day] << "---&&&---"<<day << endl;
+            day++;
+        }
+        // cout << y.ques << " &&  "<<y.exam << " &&  "<<r << " && "<<y.ID<<endl;
+        if(r > 0)
+        {
             cout << "-1\n";
             return 0;
         }
     }
-
-    for(int i = 1; i <= n; i++){
-        cout << ans[i] << " ";
+    for(auto x: Exam)
+    {
+        res[x] = m+1;
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        cout << res[i] << " ";
     }
     return 0;
 }
+
