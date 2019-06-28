bc02bec1139d55dea064eea5fafa4b30ef755ef5
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-06-28 19:51:00 +0600

    Add solution for problem `C - Computer Game` Link: https://codeforces.com/contest/1183/problem/C Tags: binary search, math, *1400 Ref: https://codeforces.com/contest/1183/submission/56229746

---
+++ codeforces/1183/C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1<<30
+#define endl '\n'
+#define maxn 100005
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+#define dbg(x) cerr << #x << " = " << x << endl;
+#define dbg2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl;
+#define dbg3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+
+const ll mod = 1e9 + 7;
+
+
+int main()
+{
+    FASTIO
+    ///*
+    //double start_time = clock();
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+//*/
+    int T;
+    cin >> T;
+    //T = 1;
+    for(int cs = 1; cs <= T; cs++)
+    {
+      ll k, n, a, b;
+      cin >> k >> n >> a >> b;
+      ll low = 0, high = n, ans = -1;
+      while(low <= high){
+        ll mid = (low + high) / 2;
+        ll x = mid * a;
+        ll y = (n - mid) * b;
+        if(x + y < k) ans = mid, low = mid + 1;
+        else high = mid - 1;
+      }
+      cout << ans << endl;
+    }
+
+    //double end_time = clock();
+    //printf( "Time = %lf ms\n", ( (end_time - start_time) / CLOCKS_PER_SEC)*1000);
+    return 0;
+}
