8a0317ae2ec094f43cb3db03d561ac5e9924cc90
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-07-08 14:45:00 +0600

    Add solution for problem `E2 - Median on Segments (General Case Edition)` Link: https://codeforces.com/contest/1005/problem/E2 Tags: sortings, *2400 Ref: https://codeforces.com/contest/1005/submission/56700168

--- codeforces/1005/E2.cpp
+++ codeforces/1005/E2.cpp
@@ -14,50 +14,50 @@ int n;
 vector<int> a;
 ll solve(int m)
 {
-    map<int, int>v;
-    int sum = n;
-    ll ans = 0;
-    v[sum] = 1;
-    ll res = 0;
-    for (int i = 0; i < n; i++) {
-        if (a[i] < m) {
-            sum--;
-            res -= v[sum];
-        }
-        else {
-            res += v[sum];
-            sum++;
-        }
-        ans += res;
-        v[sum]++;
+  vector<int> v(2 * n + 1);
+  int sum = n;
+  ll ans = 0;
+  v[sum] = 1;
+  ll res = 0;
+  for (int i = 0; i < n; i++) {
+    if (a[i] < m) {
+      sum--;
+      res -= v[sum];
     }
-    return ans;
+    else {
+      res += v[sum];
+      sum++;
+    }
+    ans += res;
+    v[sum]++;
+  }
+  return ans;
 }
 
 
 int main()
 {
-    FASTIO
-    ///*
-    //double start_time = clock();
+  FASTIO
+  ///*
+  //double start_time = clock();
 #ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    freopen("error.txt", "w", stderr);
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
 #endif
 //*/
-    int T;
-    //cin >> T;
-    T = 1;
-    for (int cs = 1; cs <= T; cs++) {
-        int m;
-        cin >> n >> m;
-        a = vector<int> (n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-        cout << solve(m) - solve(m + 1) << endl;
-    }
+  int T;
+  //cin >> T;
+  T = 1;
+  for (int cs = 1; cs <= T; cs++) {
+    int m;
+    cin >> n >> m;
+    a = vector<int> (n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    cout << solve(m) - solve(m + 1) << endl;
+  }
 
-    //double end_time = clock();
-    //printf( "Time = %lf ms\n", ( (end_time - start_time) / CLOCKS_PER_SEC)*1000);
-    return 0;
+  //double end_time = clock();
+  //printf( "Time = %lf ms\n", ( (end_time - start_time) / CLOCKS_PER_SEC)*1000);
+  return 0;
 }
