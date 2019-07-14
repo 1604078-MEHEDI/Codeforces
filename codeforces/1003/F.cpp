300e6a3d4e3a132644075bc98c5f2a24151dff86
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-07-14 12:40:00 +0600

    Add solution for problem `F - Abbreviation` Link: https://codeforces.com/contest/1003/problem/F Tags: dp, hashing, strings, *2200 Ref: https://codeforces.com/contest/1003/submission/57005755

--- codeforces/1003/F.cpp
+++ codeforces/1003/F.cpp
@@ -2,49 +2,17 @@
 using namespace std;
 #define INF 1<<30
 #define endl '\n'
-#define maxn 1005
+#define maxn 305
 #define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 typedef long long ll;
 const double PI = acos(-1.0);
 #define dbg(x) cerr << #x << " = " << x << endl;
 #define dbg2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl;
 #define dbg3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
-int n, sum;
-string s[maxn];
-pair<ll, int> ar[maxn];
 
-pair<ll, int> Hash(string str)
-{
-  ll ret  = 0, cnt = 1;
-  for (auto ch : str) {
-    ll x = ch - 'a';
-    x++;
-    ret += (x * cnt);
-    cnt *= 31;
-  }
-  return {ret, str.size()};
-}
-
-vector<pair<int, int > > mat[maxn];
+bool eq[maxn][maxn];
 int dp[maxn][maxn];
-
-int solve()
-{
-  for (int pos = 0; pos < n; pos++) {
-    for (auto x : mat[pos])
-      for (int i = 0; i < n; i++) {
-        if (!i) dp[pos][i + 1] = max(dp[pos][i + 1], x.second);
-        else dp[pos][i + 1] = max(dp[pos][i + 1], min(dp[x.first][i], x.second));
-      }
-  }
-
-  int ret = 0;
-  for (int i = 0; i <= n; i++) {
-    for (int j = 0; j <= n; j++)
-      ret = max(ret, dp[i][j] * (j + 1));
-  }
-  return ret;
-}
+string s[maxn];
 
 int main()
 {
@@ -61,33 +29,51 @@ int main()
   //cin >> T;
   T = 1;
   for (int cs = 1; cs <= T; cs++) {
+    int n;
     cin >> n;
-    sum = n - 1;
-    for (int i = 0; i < n; i++) {
+    int allSum = n - 1;
+
+    for (int i = 0; i < n ; i++) {
       cin >> s[i];
-      ar[i] = Hash(s[i]);
-      //cerr << ar[i].first << " " << ar[i].second << endl;
-      sum += ar[i].second;
+      allSum += (int)s[i].size();
     }
 
     for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        int x = i, y = j, z = 0;
-        while (x < j && y < n) {
-          if (s[x] == s[y]) {
-            if (x != i) z++;
-            z += (ar[x].second - 1);
-          }
-          else break;
-
-          x++;
-          y++;
-        }
-       // cerr << j << " --> {" <<i << " , "<<j << "} " << s[i] << " " << s[j] << endl; 
-        mat[j].push_back({i, z});
+      eq[i][i] = true;
+      for (int j = 0; j < i; j++) {
+        eq[i][j] = eq[j][i] = (s[i] == s[j]);
       }
     }
-    cout << sum - solve() << endl;
+
+    for (int i = n - 1; i >= 0; i--) {
+      for (int j = n - 1; j >= 0; j--) {
+        if (eq[i][j]) {
+          if (i + 1 < n && j + 1 < n)
+            dp[i][j] = dp[i + 1][j + 1] + 1;
+          else dp[i][j] = 1;
+        }
+      }
+    }
+
+    int ans = allSum;
+    for (int i = 0; i < n; i++) {
+      int sum = 0;
+      for (int j = 0; i + j < n; j++) {
+        sum += s[i + j].size();
+        int cnt = 1;
+        for (int pos = i + j + 1; pos < n; pos++) {
+          if (dp[i][pos] > j) {
+            cnt++;
+            pos += j;
+          }
+        }
+        int cur = allSum - sum * cnt + (j + 1) * cnt - j * cnt;
+        if (cnt > 1 && ans > cur) {
+          ans = cur;
+        }
+      }
+    }
+    cout << ans << endl;
   }
 
   //double end_time = clock();
