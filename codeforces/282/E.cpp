f24e48366f6f3bf78bbcd4cf14973226e83d81d9
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-10-21 20:25:00 +0600

    Add solution for problem `E - Sausage Maximization` Link: https://codeforces.com/contest/282/problem/E Tags: bitmasks, data structures, trees, *2200 Ref: https://codeforces.com/contest/282/submission/63078586

--- codeforces/282/E.cpp
+++ codeforces/282/E.cpp
@@ -16,37 +16,37 @@ const double PI = acos(-1.0);
 
 template < typename F, typename S >
 ostream& operator << ( ostream& os, const pair< F, S > & p ) {
-  return os << "(" << p.first << ", " << p.second << ")";
+    return os << "(" << p.first << ", " << p.second << ")";
 }
 
 template < typename T >
 ostream &operator << ( ostream & os, const vector< T > &v ) {
-  os << "{";
-  for (auto it = v.begin(); it != v.end(); ++it) {
-    if ( it != v.begin() ) os << ", ";
-    os << *it;
-  }
-  return os << "}";
+    os << "{";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
 }
 
 template < typename T >
 ostream &operator << ( ostream & os, const set< T > &v ) {
-  os << "[";
-  for (auto it = v.begin(); it != v.end(); ++it) {
-    if ( it != v.begin()) os << ", ";
-    os << *it;
-  }
-  return os << "]";
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin()) os << ", ";
+        os << *it;
+    }
+    return os << "]";
 }
 
 template < typename F, typename S >
 ostream &operator << ( ostream & os, const map< F, S > &v ) {
-  os << "[";
-  for (auto it = v.begin(); it != v.end(); ++it) {
-    if ( it != v.begin() ) os << ", ";
-    os << it -> first << " = " << it -> second ;
-  }
-  return os << "]";
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
 }
 
 #define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
@@ -58,8 +58,8 @@ void faltu () { cerr << endl; }
 
 template <typename T>
 void faltu( T a[], int n ) {
-  for (int i = 0; i < n; ++i) cerr << a[i] << ' ';
-  cerr << endl;
+    for (int i = 0; i < n; ++i) cerr << a[i] << ' ';
+    cerr << endl;
 }
 
 template <typename T, typename ... hello>
@@ -79,73 +79,72 @@ typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
 /**___________________________________________________**/
 
 bool Check(ll num, ll pos) {
-  return (bool) (num & (1ll << pos));
+    return (bool) (num & (1ll << pos));
 }
 
 ll id = 1;
 ll Trie[50 * maxn + 5][2], mark[50 * maxn + 5];
 
-void Insert(ll x) {
-  //dbg(id);
-  ll cur = 1;
-  for (ll i = 49; i >= 0; i--) {
-     ll nxt = (x >> i) & 1ll;
-    if (!Trie[cur][nxt]) Trie[cur][nxt] = ++id;
-    cur = Trie[cur][nxt];
-  }
-}
-
-ll Search(ll x)
+void Insert(ll num)
 {
-  ll cur = 1, val = 0;
-  for (ll i = 49; i >= 0; i--) {
-    ll nxt = (x >> i) & 1ll;
-    if (Trie[cur][1 - nxt]) {
-      val += (1ll << i);
-      cur = Trie[cur][1 - nxt];
+    ll row = 1;
+    for (ll i = 49; i >= 0; i--) {
+        ll d = Check(num, i);
+        if (Trie[row][d] == 0) {
+            Trie[row][d] = ++id;
+        }
+        row = Trie[row][d];
     }
-    else cur = Trie[cur][nxt];
-
-  }
-  return val;
 }
 
-
+ll Search(ll num)
+{
+    ll row = 1, val = 0;
+    for (ll i = 49; i >= 0; i--) {
+        ll d = Check(num, i);
+        if (Trie[row][1 - d] > 0) {
+            val += (1ll << i);
+            row = Trie[row][1 - d];
+        }
+        else row = Trie[row][d];
+    }
+    return val;
+}
 
 ll n;
-ll a, Pref[maxn];
+ll a[maxn], S[maxn];
 
 int main()
 {
-  FASTIO
-  ///*
+    FASTIO
+    ///*
 #ifndef ONLINE_JUDGE
-  freopen("in.txt", "r", stdin);
-  freopen("out.txt", "w", stdout);
-  freopen("error.txt", "w", stderr);
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
 #endif
 //*/
-  int T;
-  //scanf("%d", &T);
-  T = 1;
-  for (int cs = 1; cs <= T; cs++) {
-    cin >> n;
-    ll X = 0;
-    for (int i = 1; i <= n; i++) {
-      cin >> a;
-      X ^= a;
-      Pref[i] = X;
-    }
+    int T;
+    //scanf("%d", &T);
+    T = 1;
+    for (int cs = 1; cs <= T; cs++) {
+        cin >> n;
+        ll X = 0;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            X ^= a[i];
+            S[i] = X;
+        }
 
-    ll ans = X;
-    Insert(0);
-    for (int i = 1; i <= n; i++) {
-      ll got = X ^ Pref[i];
-      ll val = Search(got);
-      ans = max(ans, val);
-      Insert(Pref[i]);
+        ll ans = X;
+        Insert(0);
+        for (int i = 1; i <= n; i++) {
+            ll got = X ^ S[i];
+            ll val = Search(got);
+            ans = max(ans, val);
+            Insert(S[i]);
+        }
+        cout << ans << endl;
     }
-    cout << ans << endl;
-  }
-  return 0;
+    return 0;
 }
