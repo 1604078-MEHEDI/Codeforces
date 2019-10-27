16438911b4201ebb9e28f13f374bfa03801485ef
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-10-27 22:43:00 +0600

    Add solution for problem `D - Power Products` Link: https://codeforces.com/contest/1247/problem/D Tags: number theory, *1800 Ref: https://codeforces.com/contest/1247/submission/63609833

---
+++ codeforces/1247/D.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1<<30
+#define endl '\n'
+#define maxn 100005
+#define tc printf("Case %d: ", cs)
+#define tcn printf("Case %d:\n", cs);
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+
+#define dbg1(x) cerr << #x << " = " << x << endl;
+#define dbg2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl;
+#define dbg3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+#define dbg4(w,x, y, z) cerr << #w << " = " << w << ", " <<#x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin()) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+
+void faltu () { cerr << endl; }
+
+template <typename T>
+void faltu( T a[], int n ) {
+    for (int i = 0; i < n; ++i) cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+#include <iostream>
+using namespace __gnu_pbds;
+using namespace std;
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+
+/**___________________________________________________**/
+
+typedef pair<int, int> pii;
+int n, k;
+const int N = 1e5 + 5;
+map<vector<pii>, ll> mp;
+
+void pf(int n, vector<pii>& v)
+{
+    v.emplace_back(0, 0);
+    if (n % 2 == 0) {
+        int cnt = 0;
+        while (n % 2 == 0) {
+            n /= 2;
+            cnt++;
+        }
+        cnt %= k;
+        if (cnt)v.emplace_back(2, cnt);
+    }
+
+    for (int x = 3; x * x <= n; x += 2) {
+        if (n % x == 0) {
+            int cnt = 0;
+            while (n % x == 0) {
+                n /= x;
+                cnt++;
+            }
+            cnt %= k;
+            if (cnt)v.emplace_back(x, cnt);
+        }
+    }
+    if (n > 1) v.emplace_back(n, 1);
+}
+
+int main()
+{
+    FASTIO
+    ///*
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+//*/
+    cin >> n >> k;
+    ll ans = 0;
+    while (n--) {
+        vector<pii> v, v2;
+        int x;
+        cin >> x;
+        pf(x, v);
+        v2.emplace_back(0, 0);
+        for (int i = 1; i < (int)v.size();  i++) {
+            v2.emplace_back(v[i].first, k - v[i].second);
+        }
+        ans += mp[v2];
+        mp[v]++;
+    }
+    cout << ans << endl;
+}
