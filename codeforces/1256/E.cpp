ff331985bd91514646f8e5cdebb34d3bd89129ce
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-11-15 09:07:00 +0600

    Add solution for problem `E - Yet Another Division Into Teams` Link: https://codeforces.com/contest/1256/problem/E Tags: dp, greedy, sortings, *2000 Ref: https://codeforces.com/contest/1256/submission/64914658

---
+++ codeforces/1256/E.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1<<30
+#define endl '\n'
+#define maxn 100005
+#define tc printf("Case %d: ", cs)
+#define tcn printf("Case %d:\n", cs);
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+
+#define dbg1(x) cerr << #x << " = " << x << endl;
+#define dbg2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl;
+#define dbg3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+#define dbg4(w,x, y, z) cerr << #w << " = " << w << ", " <<#x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+	return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+	os << "{";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin() ) os << ", ";
+		os << *it;
+	}
+	return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+	os << "[";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin()) os << ", ";
+		os << *it;
+	}
+	return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+	os << "[";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin() ) os << ", ";
+		os << it -> first << " = " << it -> second ;
+	}
+	return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+
+void faltu () { cerr << endl; }
+
+template <typename T>
+void faltu( T a[], int n ) {
+	for (int i = 0; i < n; ++i) cerr << a[i] << ' ';
+	cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+#include <iostream>
+using namespace __gnu_pbds;
+using namespace std;
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+
+/**___________________________________________________**/
+
+const int N = 2e5 + 5;
+const int inf = 1e9 + 7;
+int n;
+int dp[N];
+int res[N];
+pair<int, int> a[N];
+int mx;
+int solve(int pos)
+{
+	if (pos == n)return 0;
+	if (dp[pos] != -1) return dp[pos];
+	dp[pos] = inf;
+	for (int i = pos + 3; i <= min(n, pos + 5); i++)
+		dp[pos] = min(dp[pos], a[i - 1].first - a[pos].first + solve(i));
+	return dp[pos];
+}
+
+void path(int pos, int cnt)
+{
+	if (pos == n) {
+		 mx = cnt - 1;
+		return;
+	}
+	for (int i = pos + 3; i <= min(n, pos + 5); i++)
+		if (a[i - 1].first - a[pos].first + solve(i) == dp[pos]) {
+			for (int j = pos; j < i; j++)
+				res[a[j].second] = cnt;
+			path(i, cnt + 1);
+			return;
+		}
+	return;
+}
+int main()
+{
+	FASTIO
+	///*
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+//*/
+	cin >> n;
+	memset(dp, -1, sizeof dp);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i].first;
+		a[i].second = i;
+	}
+	sort(a, a + n);
+	solve(0);
+	path(0, 1);
+	cout << dp[0] << " " << mx << endl;
+	for (int i = 0; i < n; i++)
+		cout << res[i] << " ";
+
+}
