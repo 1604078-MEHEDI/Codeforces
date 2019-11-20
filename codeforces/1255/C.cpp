1adb94538017831a1486d2f2f06614097ac71e8b
Mehedi Hasan <hasanmahadi877@gmail.com>
2019-11-20 08:04:00 +0600

    Add solution for problem `C - League of Leesins` Link: https://codeforces.com/contest/1255/problem/C Tags: constructive algorithms, implementation, *1600 Ref: https://codeforces.com/contest/1255/submission/65404363

---
+++ codeforces/1255/C.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1<<30
+#define endl '\n'
+#define maxn 100005
+#define tc printf("Case %d: ", cs)
+#define tcn printf("Case %d:\n", cs);
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+
+#define dbg1(x) cerr << #x << " = " << x << endl;
+#define dbg2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl;
+#define dbg3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+#define dbg4(w,x, y, z) cerr << #w << " = " << w << ", " <<#x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+	return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+	os << "{";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin() ) os << ", ";
+		os << *it;
+	}
+	return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+	os << "[";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin()) os << ", ";
+		os << *it;
+	}
+	return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+	os << "[";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin() ) os << ", ";
+		os << it -> first << " = " << it -> second ;
+	}
+	return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+
+void faltu () { cerr << endl; }
+
+template <typename T>
+void faltu( T a[], int n ) {
+	for (int i = 0; i < n; ++i) cerr << a[i] << ' ';
+	cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+#include <iostream>
+using namespace __gnu_pbds;
+using namespace std;
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+
+/**___________________________________________________**/
+
+vector<pair<int, int> > tup[maxn];
+int cnt[maxn];
+bool vis[maxn];
+
+void bfs(int s)
+{
+	queue<int> Q;
+	Q.push(s);
+	while (!Q.empty()) {
+		int u = Q.front();
+		Q.pop();
+		if (vis[u]) continue;
+		cout << u << " ";
+		vis[u] = 1;
+		for (auto v : tup[u]) {
+			int a = v.first;
+			int b = v.second;
+			cnt[a]--;
+			cnt[b]--;
+			if (cnt[a]  >= 0 && cnt[b] >= 0) {
+				if (cnt[a] > cnt[b]) swap(a, b);
+				Q.push(a);
+				Q.push(b);
+			}
+			else if(cnt[a] >= 0){
+				Q.push(a);
+			}
+			else if(cnt[b] >= 0) Q.push(b);
+		}
+	}
+}
+
+int main()
+{
+	FASTIO
+	///*
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+//*/
+	int n;
+	cin >> n;
+	for (int i = 0; i < n - 2; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		cnt[a]++;
+		cnt[b]++;
+		cnt[c]++;
+		tup[a].push_back({b, c});
+		tup[b].push_back({a, c});
+		tup[c].push_back({a, b});
+	}
+	int root;
+	for (int i = 1; i <= n; i++) {
+		if (cnt[i] == 1) {
+			root = i;
+			break;
+		}
+	}
+	bfs(root);
+}
