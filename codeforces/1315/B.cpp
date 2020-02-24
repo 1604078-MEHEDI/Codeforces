228ce04bf1571311cdc1b81b64fa06bb25fcff13
Mehedi Hasan <hasanmahadi877@gmail.com>
2020-02-24 13:23:00 +0600

    Add solution for problem `B - Homecoming` Link: https://codeforces.com/contest/1315/problem/B Tags: binary search, dp, greedy, strings, *1300 Ref: https://codeforces.com/contest/1315/submission/71749162

---
+++ codeforces/1315/B.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1<<30
+#define endl '\n'
+#define maxn 1000005
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+const ll mod = 1e9 + 7;
+
+inline void normal(ll &a) { a %= mod; (a < 0) && (a += mod); }
+inline ll modMul(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a * b) % mod; }
+inline ll modAdd(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a + b) % mod; }
+inline ll modSub(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while (p) { if (p & 1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, mod - 2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+
+///**
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+  return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+  os << "{";
+  for (auto it = v.begin(); it != v.end(); ++it) {
+    if ( it != v.begin() ) os << ", ";
+    os << *it;
+  }
+  return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+  os << "[";
+  for (auto it = v.begin(); it != v.end(); ++it) {
+    if ( it != v.begin()) os << ", ";
+    os << *it;
+  }
+  return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+  os << "[";
+  for (auto it = v.begin(); it != v.end(); ++it) {
+    if ( it != v.begin() ) os << ", ";
+    os << it -> first << " = " << it -> second ;
+  }
+  return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+
+void faltu () { cerr << endl; }
+
+template <typename T>
+void faltu( T a[], int n ) {
+  for (int i = 0; i < n; ++i) cerr << a[i] << ' ';
+  cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+#include <iostream>
+using namespace __gnu_pbds;
+using namespace std;
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+
+// find_by_order(k) – ফাংশনটি kth ordered element এর একটা পয়েন্টার রিটার্ন করে। অর্থাৎ তুমি চাইলেই kth ইন্ডেক্সে কি আছে, সেটা জেনে ফেলতে পারছো!
+// order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
+
+//*//**___________________________________________________**/
+
+string s;
+int a, b, p;
+
+bool can(int idx) {
+  int n = s.length();
+  //dbg(a, b, p, n);
+  ll ret = 0;
+  char last = 'M';
+  for (int i = idx; i < n - 1; i++) {
+    if (s[i] == last)continue;
+    if (s[i] == 'A') ret += a;
+    else ret += b;
+    last = s[i];
+  }
+  return p >= ret;
+}
+
+int main()
+{
+  FASTIO
+  ///*
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+#endif
+//*/
+  int T;
+  //scanf("%d", &T);
+  T = 1;
+  cin >> T;
+  for (int cs = 1; cs <= T; cs++) {
+    cin >> a >> b >> p;
+
+    cin >> s;
+    int n = s.length();
+    if (n == 2) {
+      if (s[0] == 'A') {
+        if (p >= a) cout << "1\n";
+        else cout << "2\n";
+      }
+      else {
+        if (p >= b) cout << "1\n";
+        else cout << "2\n";
+      }
+      continue;
+    }
+    int l = 0, r = n;
+    int ans = 1;
+    while (l <= r) {
+      int m = (l + r) / 2;
+      // dbg(m);
+      if (can(m)) {
+        ans = m;
+        //dbg(m);
+        r = m - 1;
+      }
+      else {
+        l = m + 1;
+      }
+    }
+    cout << ans + 1 << endl;
+  }
+  return 0;
+}
