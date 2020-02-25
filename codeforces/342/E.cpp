9a8f8e083e0278dd3d02c1f8cf76317fa0e7f6fa
Mehedi Hasan <hasanmahadi877@gmail.com>
2020-02-25 13:16:00 +0600

    Add solution for problem `E - Xenia and Tree` Link: https://codeforces.com/contest/342/problem/E Tags: data structures, divide and conquer, trees, *2400 Ref: https://codeforces.com/contest/342/submission/71840576

--- codeforces/342/E.cpp
+++ codeforces/342/E.cpp
@@ -1,17 +1,12 @@
-/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم*/
-
-//#pragma GCC optimize("O3,unroll-loops")
-//#pragma GCC target("avx,avx2,fma")
-
 #include <bits/stdc++.h>
 using namespace std;
+#define INF 1<<30
+#define endl '\n'
+#define maxn 1000005
 #define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 typedef long long ll;
-using pii = pair<int, int>;
 const double PI = acos(-1.0);
 const ll mod = 1e9 + 7;
-//const ll mod = 998244353;
-
 
 inline void normal(ll &a) { a %= mod; (a < 0) && (a += mod); }
 inline ll modMul(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a * b) % mod; }
@@ -21,189 +16,221 @@ inline ll modPow(ll b, ll p) { ll r = 1; while (p) { if (p & 1) r = modMul(r, b)
 inline ll modInverse(ll a) { return modPow(a, mod - 2); }
 inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
 
-#define si(x) scanf("%d",&x)
-#define sii(x,y) scanf("%d %d",&x,&y)
-#define siii(x,y,z) scanf("%d %d %d",&x,&y,&z)
-#define sl(x) scanf("%lld",&x)
-#define sll(x,y) scanf("%lld %lld",&x,&y)
-#define slll(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
-#define ss(ch) scanf("%s",ch)
-#define pi(x) printf("%d",x)
-#define pii(x,y) printf("%d %d",x,y)
-#define piii(x,y,z) printf("%d %d %d",x,y,z)
-#define pl(x) printf("%lld",x)
-#define pll(x,y) printf("%lld %lld",x,y)
-#define plll(x,y,z) printf("%lld %lld %lld",x,y,z)
-#define ps(ch) printf("%s",ch)
-#define F(i,a,b)      for(int i= a; i <= b; i++)
-#define R(i,b,a)      for(int i= b; i >= a; i--)
-#define REP(i,n) for(int i = 0; i < (n); i++)
-#define sline(a) scanf("%[^\n]s",a)
-#define Case(t) printf("Case %d:\n",t)
 
-int dx[] = {1, -1, 0, 0};
-int dy[] = {0, 0, 1, -1};
-int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
-int dy8[] = {1, -1, -1, 0, 1, -1, 0, 1};
-int kx8[] = {1, 1, 2, 2, -1, -1, -2, -2};
-int ky8[] = {2, -2, 1, -1, 2, -2, 1, -1};
-/* for Random Number generate
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-*/
 ///**
-template < typename F, typename S >ostream& operator << ( ostream& os, const pair< F, S > & p ) {return os << "(" << p.first << ", " << p.second << ")";}
-template < typename T >ostream &operator << ( ostream & os, const vector< T > &v ) {os << "{"; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << *it;} return os << "}";}
-template < typename T >ostream &operator << ( ostream & os, const set< T > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin()) os << ", "; os << *it;} return os << "]";}
-template < typename F, typename S >ostream &operator << ( ostream & os, const map< F, S > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << it -> first << " = " << it -> second ;} return os << "]";}
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+	return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+	os << "{";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin() ) os << ", ";
+		os << *it;
+	}
+	return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+	os << "[";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin()) os << ", ";
+		os << *it;
+	}
+	return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+	os << "[";
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if ( it != v.begin() ) os << ", ";
+		os << it -> first << " = " << it -> second ;
+	}
+	return os << "]";
+}
+
 #define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
 clock_t tStart = clock();
 #define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+
 void faltu () { cerr << endl; }
-template <typename T>void faltu( T a[], int n ) {for (int i = 0; i < n; ++i) cerr << a[i] << ' '; cerr << endl;}
+
+template <typename T>
+void faltu( T a[], int n ) {
+	for (int i = 0; i < n; ++i) cerr << a[i] << ' ';
+	cerr << endl;
+}
+
 template <typename T, typename ... hello>
 void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
-
 // Program showing a policy-based data structure.
 #include <ext/pb_ds/assoc_container.hpp> // Common file 
 #include <ext/pb_ds/tree_policy.hpp>
 #include <functional> // for less 
+#include <iostream>
 using namespace __gnu_pbds;
-
+using namespace std;
 // GNU link : https://goo.gl/WVDL6g
 typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
         tree_order_statistics_node_update>
         new_data_set;
+
 // find_by_order(k) – ফাংশনটি kth ordered element এর একটা পয়েন্টার রিটার্ন করে। অর্থাৎ তুমি চাইলেই kth ইন্ডেক্সে কি আছে, সেটা জেনে ফেলতে পারছো!
 // order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
+
 //*//**___________________________________________________**/
-const int N = 100006;
+
+const int N = 1e5 + 10;
 const int LOGN = 20;
+const int inf = 1e9;
 set<int> g[N];
 int par[N], sub[N], level[N], ans[N];
 int dp[LOGN][N];
 int n, m;
 
-void dfs(int u) {
-    for (auto &v : g[u]) {
-        if (v != dp[0][u]) {
-            dp[0][v] = u;
-            level[v] = level[u] + 1;
-            dfs(v);
-        }
-    }
+/*----------- Pre-Processing ------------*/
+
+void dfs0(int u)
+{
+	for (auto it = g[u].begin(); it != g[u].end(); it++) {
+		if (*it != dp[0][u]) {
+			dp[0][*it] = u;
+			level[*it] = level[u] + 1;
+			dfs0(*it);
+		}
+	}
 }
 
-void preprocess() {
-    level[0] = 0;
-    dp[0][0] = 0;
-    dfs(0);
-    for (int i = 1; i < LOGN; i++)
-        for (int j = 0; j < n; j++)
-            dp[i][j] = dp[i - 1][dp[i - 1][j]];
+void preprocess()
+{
+	level[0] = 0;
+	dp[0][0] = 0;
+	dfs0(0);
+	for (int i = 1; i < LOGN; i++)
+		for (int j = 0; j < n; j++)
+			dp[i][j] = dp[i - 1][dp[i - 1][j]];
 }
 
-int lca(int a, int b) {
-    if (level[a] > level[b])swap(a, b);
-    int d = level[b] - level[a];
-    for (int i = 0; i < LOGN; i++)
-        if (d & (1 << i)) b = dp[i][b];
-    if (a == b) return a;
-    for (int i = LOGN - 1; i >= 0; i--)
-        if (dp[i][a] != dp[i][b]) {
-            a = dp[i][a];
-            b = dp[i][b];
-        }
-    return dp[0][a];
+int lca(int a, int b)
+{
+	if (level[a] > level[b]) swap(a, b);
+	int d = level[b] - level[a];
+	for (int i = 0; i < LOGN; i++)
+		if (d & (1 << i)) {
+			b = dp[i][b];
+		}
+
+	if (a == b) return a;
+	for (int i = LOGN - 1; i >= 0; i--)
+		if (dp[i][a] != dp[i][b]) {
+			a = dp[i][a];
+			b = dp[i][b];
+		}
+	return dp[0][a];
 }
 
-int dist(int a, int b) {
-    return level[a] + level[b] - 2 * level[lca(a, b)];
+int dist(int u, int v)
+{
+	return level[u] + level[v] - 2 * level[lca(u, v)];
 }
-
-
+/*-----------------Decomposition Part--------------------------*/
 int cnt;
-void get_sub(int u, int p) {
-    sub[u] = 1;
-    cnt++;
-    for (auto &v : g[u]) {
-        if (v != p) {
-            get_sub(v, u);
-            sub[u] += sub[v];
-        }
-    }
+void dfs1(int u, int p)
+{
+	sub[u] = 1;
+	cnt++;
+	for (auto v = g[u].begin(); v != g[u].end(); v++) {
+		if (*v != p) {
+			dfs1(*v, u);
+			sub[u] += sub[*v];
+		}
+	}
 }
 
-int Centroid(int u, int p) {
-    for (auto &v : g[u]) {
-        if (v != p && sub[v] > cnt / 2)
-            return Centroid(v, u);
-    }
-    return u;
+int dfs2(int u, int p)
+{
+	for (auto v = g[u].begin(); v != g[u].end(); v++) {
+		if (*v != p && sub[*v] > cnt / 2)
+			return dfs2(*v, u);
+	}
+	return u;
 }
 
-void decompose(int root, int p) {
-    cnt = 0;
-    get_sub(root, root);
-    int C = Centroid(root, root);
-    if (p == -1)p = C;
-    par[C] = p;
-    for (auto &v : g[C]) {
-        g[v].erase(C);
-        decompose(v, C);
-    }
-    // g[C].clear();
+void decompose(int root, int p)
+{
+	cnt = 0;
+	dfs1(root, root);
+	int centroid = dfs2(root, root);
+	if (p == -1) p = centroid;
+	par[centroid] = p;
+	for (auto v = g[centroid].begin(); v != g[centroid].end(); v++) {
+		g[*v].erase(centroid);
+		decompose(*v, centroid);
+	}
+	g[centroid].clear();
+}
+
+/*----------------- Handle the Queries -----------------*/
+
+void update(int u)
+{
+	int x = u;
+	while (1) {
+		ans[x] = min(ans[x], dist(x, u));
+		if (x == par[x]) break;
+		x = par[x];
+	}
+}
+
+int query(int u)
+{
+	int x = u;
+	int ret = inf;
+	while (1) {
+		ret = min(ret, dist(u, x) + ans[x]);
+		if (x == par[x]) break;
+		x = par[x];
+	}
+	return ret;
 }
 
 
-
-
-void update(int u) {
-    int x = u;
-    while (1) {
-        ans[x] = min(ans[x], dist(x, u));
-        if (x == par[x])break;
-        x = par[x];
-    }
-}
-
-int query(int u) {
-    int x = u;
-    int ret = mod;
-    while (1) {
-        ret = min(ret, dist(u, x) + ans[x]);
-        if (x == par[x]) break;
-        x = par[x];
-    }
-    return ret;
-}
 
 int main()
 {
-    FASTIO
-    ///*
+	//FASTIO
+	///*
 #ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    freopen("error.txt", "w", stderr);
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
 #endif
 //*/
-    cin >> n >> m;
-    for (int i = 1; i < n; i++) {
-        int a, b;
-        cin >> a >> b;
-        --a; --b;
-        g[a].insert(b);
-        g[b].insert(a);
-    }
-    preprocess();
-    decompose(0, -1);
-    for (int i = 0; i < n; i++)ans[i] = mod;
-    update(0);
-    for (int i = 0; i < m; i++) {
-        int ty, v;
-        cin >> ty >> v;
-        --v;
-        if (ty == 1)update(v);
-        else cout << query(v) << "\n";
-    }
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i < n; i++) {
+		int u, v;
+		scanf("%d %d", &u, &v);
+		--u;
+		--v;
+		g[u].insert(v);
+		g[v].insert(u);
+	}
+	preprocess();
+	decompose(0, -1);
+	for (int i = 0; i < n; i++) ans[i] = inf;
+	update(0);// first node is initially painted red
+	for (int i = 0; i < m; i++) {
+		int t, v;
+		scanf("%d%d", &t, &v);
+		//dbg(t, v);
+		--v;
+		if (t == 1) update(v);
+		else printf("%d\n", query(v));
+	}
+	return 0;
 }
