f4b0c62e0418c2781c8c054f7e23cf3465121afa
Mehedi Hasan <hasanmahadi877@gmail.com>
2020-04-23 00:49:00 +0600

    Add solution for problem `E - Duff in the Army` Link: https://codeforces.com/contest/588/problem/E Tags: data structures, dfs and similar, trees, *2200 Ref: https://codeforces.com/contest/588/submission/77686861

---
+++ codeforces/588/E.cpp
@@ -0,0 +1,187 @@
+/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم*/
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+const ll mod = 1e9 + 7;
+//const ll mod = 998244353;
+
+inline void normal(ll &a) { a %= mod; (a < 0) && (a += mod); }
+inline ll modMul(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a * b) % mod; }
+inline ll modAdd(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a + b) % mod; }
+inline ll modSub(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while (p) { if (p & 1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, mod - 2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+#define si(x) scanf("%d",&x)
+#define sii(x,y) scanf("%d %d",&x,&y)
+#define siii(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sl(x) scanf("%lld",&x)
+#define sll(x,y) scanf("%lld %lld",&x,&y)
+#define slll(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define ss(ch) scanf("%s",ch)
+#define pi(x) printf("%d",x)
+#define pii(x,y) printf("%d %d",x,y)
+#define piii(x,y,z) printf("%d %d %d",x,y,z)
+#define pl(x) printf("%lld",x)
+#define pll(x,y) printf("%lld %lld",x,y)
+#define plll(x,y,z) printf("%lld %lld %lld",x,y,z)
+#define ps(ch) printf("%s",ch)
+#define F(i,a,b)      for(int i= a; i <= b; i++)
+#define R(i,b,a)      for(int i= b; i >= a; i--)
+
+/* for Random Number generate
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+*/
+///**
+template < typename F, typename S >ostream& operator << ( ostream& os, const pair< F, S > & p ) {return os << "(" << p.first << ", " << p.second << ")";}
+template < typename T >ostream &operator << ( ostream & os, const vector< T > &v ) {os << "{"; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << *it;} return os << "}";}
+template < typename T >ostream &operator << ( ostream & os, const set< T > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin()) os << ", "; os << *it;} return os << "]";}
+template < typename F, typename S >ostream &operator << ( ostream & os, const map< F, S > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << it -> first << " = " << it -> second ;} return os << "]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+void faltu () { cerr << endl; }
+template <typename T>void faltu( T a[], int n ) {for (int i = 0; i < n; ++i) cerr << a[i] << ' '; cerr << endl;}
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+using namespace __gnu_pbds;
+
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+// find_by_order(k) – ফাংশনটি kth ordered element এর একটা পয়েন্টার রিটার্ন করে। অর্থাৎ তুমি চাইলেই kth ইন্ডেক্সে কি আছে, সেটা জেনে ফেলতে পারছো!
+// order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
+//*//**___________________________________________________**/
+const int N = 100006;
+const int lg = 17;
+vector<int> g[N];
+struct data
+{
+  int a[11];
+  data() {
+    memset(a, 63, sizeof a);
+  }
+  void Insert(int x) {
+    a[10] = x;
+    sort(a, a + 11);
+  }
+} vals[lg][N];
+
+data merge(data x, data y)
+{
+  data ans = x;
+  for (int i = 0; i < 11; i++)
+    ans.Insert(y.a[i]);
+  return ans;
+}
+
+int P[lg][N], d[N];
+
+void dfs(int v, int p)
+{
+  P[0][v] = p;
+  for (int i = 1; i < lg; i++) {
+    P[i][v] = P[i - 1][P[i - 1][v]];
+    vals[i][v] = merge(vals[i - 1][v], vals[i - 1][P[i - 1][v]]);
+  }
+  for (auto to : g[v]) {
+    if (to == p)continue;
+    d[to] = d[v] + 1;
+    dfs(to, v);
+  }
+}
+
+int get_parent(int v, int k)
+{
+  for (int i = 0; i < lg; i++) {
+    if ((1 << i)&k)v = P[i][v];
+  }
+  return v;
+}
+
+int lca(int u, int v)
+{
+  if (d[u] < d[v]) swap(v, u);
+  u = get_parent(u, d[u] - d[v]);
+  if (u == v)return u;
+  for (int i = lg - 1; i >= 0; i--) {
+    if (P[i][u] != P[i][v]) {
+      u = P[i][u];
+      v = P[i][v];
+    }
+  }
+  return P[0][u];
+}
+
+data get(int v, int k)
+{
+  data ans;
+  for (int i = 0; i < lg; i++) {
+    if ((1 << i)&k) {
+      ans = merge(ans, vals[i][v]);
+      v = P[i][v];
+    }
+  }
+  return ans;
+}
+
+int main()
+{
+  //FASTIO
+  ///*
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+#endif
+//*/
+  int n, m, q;
+  siii(n, m, q);
+  for (int i = 0; i < n - 1; i++) {
+    int u, v;
+    sii(u, v);
+    --v;
+    --u;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+
+  for (int i = 0; i < m; i++) {
+    int x;
+    si(x);
+    --x;
+    vals[0][x].Insert(i);
+  }
+
+  dfs(0, 0);
+  while (q--) {
+    int u, v, k;
+    siii(u, v, k);
+    --u;
+    --v;
+    int lc = lca(u, v);
+    data x = get(u, d[u] - d[lc]);
+    data y = get(v, d[v] - d[lc] + 1);
+    data ans = merge(x, y);
+    int cnt = 0;
+    //dbg(cnt);
+    while (cnt < k && ans.a[cnt] < m)cnt++;
+    printf("%d", cnt);
+    for (int i = 0; i < cnt; i++)
+      printf(" %d", ans.a[i] + 1);
+    printf("\n");
+  }
+  return 0;
+}
