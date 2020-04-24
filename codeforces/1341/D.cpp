80e34d4dac6d6da285bee4e98dcb0bf4aa9579dc
Mehedi Hasan <hasanmahadi877@gmail.com>
2020-04-24 22:38:00 +0600

    Add solution for problem `D - Nastya and Scoreboard` Link: https://codeforces.com/contest/1341/problem/D Tags: bitmasks, dp, greedy, *1700 Ref: https://codeforces.com/contest/1341/submission/77937297

---
+++ codeforces/1341/D.cpp
@@ -0,0 +1,159 @@
+/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم*/
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+const ll mod = 1e9 + 7;
+//const ll mod = 998244353;
+
+inline void normal(ll &a) { a %= mod; (a < 0) && (a += mod); }
+inline ll modMul(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a * b) % mod; }
+inline ll modAdd(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a + b) % mod; }
+inline ll modSub(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while (p) { if (p & 1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, mod - 2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+#define si(x) scanf("%d",&x)
+#define sii(x,y) scanf("%d %d",&x,&y)
+#define siii(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sl(x) scanf("%lld",&x)
+#define sll(x,y) scanf("%lld %lld",&x,&y)
+#define slll(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define ss(ch) scanf("%s",ch)
+#define pi(x) printf("%d",x)
+#define pii(x,y) printf("%d %d",x,y)
+#define piii(x,y,z) printf("%d %d %d",x,y,z)
+#define pl(x) printf("%lld",x)
+#define pll(x,y) printf("%lld %lld",x,y)
+#define plll(x,y,z) printf("%lld %lld %lld",x,y,z)
+#define ps(ch) printf("%s",ch)
+#define F(i,a,b)      for(int i= a; i <= b; i++)
+#define R(i,b,a)      for(int i= b; i >= a; i--)
+
+/* for Random Number generate
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+*/
+///**
+template < typename F, typename S >ostream& operator << ( ostream& os, const pair< F, S > & p ) {return os << "(" << p.first << ", " << p.second << ")";}
+template < typename T >ostream &operator << ( ostream & os, const vector< T > &v ) {os << "{"; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << *it;} return os << "}";}
+template < typename T >ostream &operator << ( ostream & os, const set< T > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin()) os << ", "; os << *it;} return os << "]";}
+template < typename F, typename S >ostream &operator << ( ostream & os, const map< F, S > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << it -> first << " = " << it -> second ;} return os << "]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+void faltu () { cerr << endl; }
+template <typename T>void faltu( T a[], int n ) {for (int i = 0; i < n; ++i) cerr << a[i] << ' '; cerr << endl;}
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+using namespace __gnu_pbds;
+
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+// find_by_order(k) – ফাংশনটি kth ordered element এর একটা পয়েন্টার রিটার্ন করে। অর্থাৎ তুমি চাইলেই kth ইন্ডেক্সে কি আছে, সেটা জেনে ফেলতে পারছো!
+// order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
+//*//**___________________________________________________**/
+const int N = 2006;
+const int D = 10;
+
+const ll inf = 2e18;
+const string alpha[] = { "1110111", "0010010", "1011101", "1011011", "0111010", "1101011", "1101111", "1010010", "1111111", "1111011" };
+
+int go(const string &p, const string &q)
+{
+    //dbg(p, q);
+    int ret = 0;
+    for (int i = 0; i < 7; i++) {
+        if (p[i] == '1' && q[i] == '0') return -1;
+        if (p[i] == '0' && q[i] == '1')++ret;
+    }
+    return ret;
+}
+int Change[N][D];
+int dp[N][N][11];
+int par[N][N];
+char dig[N][N];
+int n, k;
+
+int rec(int id, int sm, int num)
+{
+    int &ret = dp[id][sm][num];
+    if (id == n) {
+        if (sm == 0)return ret = 1;
+        return ret= 0;
+    }
+
+    if (sm < 0)return ret = 0;
+
+    if (~ret)return ret;
+    ret = 0;
+    for (int i = 9; i >= 0; i--) {
+        int cnt = Change[id][i];
+        // char ch = 48 + i;
+        if (cnt != -1 && cnt <= sm)
+            ret = max(ret, rec(id + 1, sm - cnt, i));
+    }
+    return ret;
+}
+
+string ans = "";
+
+void Find(int id, int sm, int num)
+{
+    // dbg(id, sm, num);
+    if (id == n)return;
+
+    for (int i = 9; i >= 0; i--) {
+        int cnt = Change[id][i];
+        // dbg(cnt);
+        // dbg(dp[id + 1][sm - cnt][i]);
+        if (sm - cnt >= 0 && cnt != -1 && dp[id + 1][sm - cnt][i] == 1) {
+            char ch = i + 48;
+            ans += ch;
+            Find(id + 1, sm - Change[id][i], i);
+            return;
+        }
+    }
+}
+
+int main()
+{
+    FASTIO
+    ///*
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+//*/
+    //int n, k;
+    cin >> n >> k;
+    vector<string > s(n);
+    for (int i = 0; i < n; i++) cin >> s[i];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < D; j++) {
+            Change[i][j] = go(s[i], alpha[j]);
+            // dbg(Change[i][j]);
+        }
+    }
+
+    memset(dp, -1, sizeof dp);
+
+    rec(0, k, 0);
+    Find(0, k, 0);
+    if (ans.size() == 0)cout << "-1\n";
+    else cout << ans << "\n";
+    return 0;
+}
