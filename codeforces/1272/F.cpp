cdca99a054c35e9cb548558b73e7142fc2428543
Mehedi Hasan <hasanmahadi877@gmail.com>
2020-05-28 14:03:00 +0600

    Add solution for problem `F - Two Bracket Sequences` Link: https://codeforces.com/contest/1272/problem/F Tags: dp, strings, two pointers, *2200 Ref: https://codeforces.com/contest/1272/submission/81683517

---
+++ codeforces/1272/F.cpp
@@ -0,0 +1,130 @@
+/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم*/
+
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+const ll mod = 1e9 + 7;
+//const ll mod = 998244353;
+
+
+inline void normal(ll &a) { a %= mod; (a < 0) && (a += mod); }
+inline ll modMul(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a * b) % mod; }
+inline ll modAdd(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a + b) % mod; }
+inline ll modSub(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while (p) { if (p & 1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, mod - 2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+#define si(x) scanf("%d",&x)
+#define sii(x,y) scanf("%d %d",&x,&y)
+#define siii(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sl(x) scanf("%lld",&x)
+#define sll(x,y) scanf("%lld %lld",&x,&y)
+#define slll(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define ss(ch) scanf("%s",ch)
+#define pi(x) printf("%d",x)
+#define pii(x,y) printf("%d %d",x,y)
+#define piii(x,y,z) printf("%d %d %d",x,y,z)
+#define pl(x) printf("%lld",x)
+#define pll(x,y) printf("%lld %lld",x,y)
+#define plll(x,y,z) printf("%lld %lld %lld",x,y,z)
+#define ps(ch) printf("%s",ch)
+#define F(i,a,b)      for(int i= a; i <= b; i++)
+#define R(i,b,a)      for(int i= b; i >= a; i--)
+
+/* for Random Number generate
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+*/
+///**
+template < typename F, typename S >ostream& operator << ( ostream& os, const pair< F, S > & p ) {return os << "(" << p.first << ", " << p.second << ")";}
+template < typename T >ostream &operator << ( ostream & os, const vector< T > &v ) {os << "{"; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << *it;} return os << "}";}
+template < typename T >ostream &operator << ( ostream & os, const set< T > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin()) os << ", "; os << *it;} return os << "]";}
+template < typename F, typename S >ostream &operator << ( ostream & os, const map< F, S > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << it -> first << " = " << it -> second ;} return os << "]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+void faltu () { cerr << endl; }
+template <typename T>void faltu( T a[], int n ) {for (int i = 0; i < n; ++i) cerr << a[i] << ' '; cerr << endl;}
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+using namespace __gnu_pbds;
+
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+// find_by_order(k) – ফাংশনটি kth ordered element এর একটা পয়েন্টার রিটার্ন করে। অর্থাৎ তুমি চাইলেই kth ইন্ডেক্সে কি আছে, সেটা জেনে ফেলতে পারছো!
+// order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
+//*//**___________________________________________________**/
+const int N = 206;
+
+string s, t;
+int dp[N][N][N + N];
+int n, m;
+
+int go(int i, int j, int sm) {
+  //dbg(n, m);
+  if (sm < 0)return mod;
+  if (i >= n && j >= m) return sm;
+  int &ret = dp[i][j][sm];
+  //dbg(ret);
+  if (ret != -1) return ret;
+  ret = mod;
+  if (sm < 405)
+    ret = min(ret, go((s[i] == '(' ? i + 1 : i), (t[j] == '(' ? j + 1 : j), sm + 1) + 1);
+  if (sm > 0)
+    ret = min(ret, go((s[i] == ')' ? i + 1 : i), (t[j] == ')' ? j + 1 : j), sm - 1) + 1);
+  return ret;
+}
+int main()
+{
+  //FASTIO
+  ///*
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+#endif
+//*/
+  cin >> s >> t;
+  n = s.length();
+  m = t.length();
+ // dbg(s, t);
+  memset(dp, -1, sizeof dp);
+
+  int i = 0, j = 0;
+  int sm = 0;
+  while (i < n || j < m) {
+    //dbg(i, j);
+    int x = mod;
+    int y = mod;
+    if (sm < 405)
+      x = go((s[i] == '(' ? i + 1 : i), (t[j] == '(' ? j + 1 : j), sm + 1) + 1;
+    if (sm > 0)
+      y = go((s[i] == ')' ? i + 1 : i), (t[j] == ')' ? j + 1 : j), sm - 1) + 1;
+    if (x < y) {
+      cout << "(";
+      if (s[i] == '(')i++;
+      if (t[j] == '(')j++;
+      sm++;
+    }
+    else {
+      cout << ")";
+      if (s[i] == ')')i++;
+      if (t[j] == ')')j++;
+      sm--;
+    }
+  }
+  while (sm--)cout << ")";
+  return 0;
+}
