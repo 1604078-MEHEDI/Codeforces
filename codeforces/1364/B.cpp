ca4836a779055d74aea810762b4a7a4501d86dfe
Mehedi Hasan <hasanmahadi877@gmail.com>
2020-06-13 23:53:00 +0600

    Add solution for problem `B - Most socially-distanced subsequence` Link: https://codeforces.com/contest/1364/problem/B Tags: greedy, two pointers, *1300 Ref: https://codeforces.com/contest/1364/submission/83686885

---
+++ codeforces/1364/B.cpp
@@ -0,0 +1,215 @@
+/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم*/
+
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+const double PI = acos(-1.0);
+const ll mod = 1e9 + 7;
+//const ll mod = 998244353;
+
+
+inline void normal(ll &a) { a %= mod; (a < 0) && (a += mod); }
+inline ll modMul(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a * b) % mod; }
+inline ll modAdd(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a + b) % mod; }
+inline ll modSub(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while (p) { if (p & 1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, mod - 2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+#define si(x) scanf("%d",&x)
+#define sii(x,y) scanf("%d %d",&x,&y)
+#define siii(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sl(x) scanf("%lld",&x)
+#define sll(x,y) scanf("%lld %lld",&x,&y)
+#define slll(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define ss(ch) scanf("%s",ch)
+#define pi(x) printf("%d",x)
+#define pii(x,y) printf("%d %d",x,y)
+#define piii(x,y,z) printf("%d %d %d",x,y,z)
+#define pl(x) printf("%lld",x)
+#define pll(x,y) printf("%lld %lld",x,y)
+#define plll(x,y,z) printf("%lld %lld %lld",x,y,z)
+#define ps(ch) printf("%s",ch)
+#define F(i,a,b)      for(int i= a; i <= b; i++)
+#define R(i,b,a)      for(int i= b; i >= a; i--)
+
+int dx[]  = {0, 0, 1, -1};
+int dy[]  = {1, -1, 0, 0};
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1, -1, -1, 0, 1, -1, 0, 1};
+int kx8[] = {1, 1, 2, 2, -1, -1, -2, -2};
+int ky8[] = {2, -2, 1, -1, 2, -2, 1, -1};
+/* for Random Number generate
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+*/
+///**
+template < typename F, typename S >ostream& operator << ( ostream& os, const pair< F, S > & p ) {return os << "(" << p.first << ", " << p.second << ")";}
+template < typename T >ostream &operator << ( ostream & os, const vector< T > &v ) {os << "{"; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << *it;} return os << "}";}
+template < typename T >ostream &operator << ( ostream & os, const set< T > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin()) os << ", "; os << *it;} return os << "]";}
+template < typename F, typename S >ostream &operator << ( ostream & os, const map< F, S > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << it -> first << " = " << it -> second ;} return os << "]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+void faltu () { cerr << endl; }
+template <typename T>void faltu( T a[], int n ) {for (int i = 0; i < n; ++i) cerr << a[i] << ' '; cerr << endl;}
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+using namespace __gnu_pbds;
+
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+// find_by_order(k) – ফাংশনটি kth ordered element এর একটা পয়েন্টার রিটার্ন করে। অর্থাৎ তুমি চাইলেই kth ইন্ডেক্সে কি আছে, সেটা জেনে ফেলতে পারছো!
+// order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
+//*//**___________________________________________________**/
+const int N = 1000006;
+
+
+int main()
+{
+	FASTIO
+	///*
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+//*/
+	int T;
+	T = 1;
+	//scanf("%d", &T);
+	cin >> T;
+	for (int cs = 1; cs <= T; cs++) {
+		int n;
+		cin >> n;
+		int mini = 2 * n, maxi = 0;
+		vector<int> a(n);
+		vector<int> s1, s2;
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		bool flag = true;
+		//int last = 0;
+
+		//dbg(a);
+		for (int i = 0; i < n;) {
+			int j = i;
+
+			if (flag) {
+				maxi = 0;
+				for (; j < n; j++) {
+					if (a[j] < mini) {
+						mini = a[j];
+						i = j + 1;
+					}
+					else break;
+				}
+				flag = false;
+			}
+			else {
+				mini = 2 * n;
+				for (; j < n; j++) {
+					if (a[j] > maxi) {
+						maxi = a[j];
+						i = j + 1;
+					}
+					else break;
+				}
+				flag = true;
+			}
+			if (flag) {
+				//dbg(maxi);
+				s1.push_back(maxi);
+			}
+			else {
+				s1.push_back(mini);
+				//dbg(mini);
+			}
+
+		}
+
+
+
+		flag = false;
+
+		mini = 2 * n;
+		maxi = 0;
+		//int last = 0;
+		for (int i = 0; i < n;) {
+			int j = i;
+
+			if (flag) {
+				maxi = 0;
+				for (; j < n; j++) {
+					if (a[j] < mini) {
+						mini = a[j];
+						i = j + 1;
+					}
+					else break;
+				}
+				flag = false;
+			}
+			else {
+				mini = 2 * n;
+				//dbg(i, j, maxi);
+				for (; j < n; j++) {
+					if (a[j] > maxi) {
+						maxi = a[j];
+						i = j + 1;
+					}
+					else break;
+				}
+
+				flag = true;
+			}
+			if (flag) {
+				//dbg(i,maxi);
+				//dbg(maxi);
+				s2.push_back(maxi);
+			}
+			else {
+				s2.push_back(mini);
+				//dbg(mini);
+			}
+
+		}
+		//dbg(s1);
+		//dbg(s2);
+		int r1 = 0, r2 = 0;
+		int last = s1[0];
+		for (auto it : s1) {
+			r1 += abs(it - last);
+			last = it;
+		}
+		last = s2[0];
+		for (auto it : s2) {
+			r2 += abs(it - last);
+			last = it;
+		}
+		if (r1 > r2) {
+			cout << s1.size() << "\n";
+			for (auto x : s1) {
+				cout << x << " ";
+			}
+			cout << "\n";
+		}
+		else {
+			cout << s2.size() << "\n";
+			for (auto x : s2) {
+				cout << x << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
