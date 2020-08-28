bf84364a04eee149f7e89143354f81303b85bda2
Mehedi Hasan <hasanmahadi877@gmail.com>
2020-08-28 15:09:00 +0600

    Add solution for problem `F - Bear and Bowling 4` Link: https://codeforces.com/contest/660/problem/F Tags: binary search, data structures, geometry, ternary search, *2500 Ref: https://codeforces.com/contest/660/submission/91194546

--- codeforces/660/F.cpp
+++ codeforces/660/F.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 #define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 typedef long long ll;
-using pii = pair<int, int>;
+using pii = pair<ll, ll>;
 const double PI = acos(-1.0);
 const ll mod = 1e9 + 7;
 //const ll mod = 998244353;
@@ -75,143 +75,38 @@ typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
 // order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
 //*//**___________________________________________________**/
 const int N = 200006;
+ll n;
+ll P[N], S[N];
+vector<ll> C, M;
+ll sz = 0;
 
-// CONVEX HULL TRICK
-
-/* Instructions
--> For Linear Version:
-1. Sort lines based on decreasing M and in case of tie, increasing B.
-2. Sort query points according to increasing X.
-3. Clear the class, add all lines and then query.
-*/
-
-
-ll dp[N], tm[N];
-ll cost[N], val[N], sm[N];
-
-inline ll getConst(ll u) {
-	return dp[u] + u * sm[u] - cost[N];
-	// constant part: *** EDIT HERE ***
+bool Bad(ll f1, ll f2, ll f3) {
+	return 1.0*(C[f1] - C[f2]) * (M[f3] - M[f1]) <= 1.0*(C[f1] - C[f3]) * (M[f2] - M[f1]);
 }
 
-
-// dp when last partition ended at u and this one ends at v
-inline ll getDP(ll u, ll v) {
-	return cost[v] - v * sm[u] + getConst(u);
-	// partition equation: *** EDIT HERE ***
+void Add(ll m, ll c) {
+	sz++;
+	M.push_back(m);
+	C.push_back(c);
+	while (sz >= 3 && Bad(sz - 3, sz - 2, sz - 1)) {
+		sz--;
+		M.erase(M.end() - 2);
+		C.erase(C.end() - 2);
+	}
 }
 
-// derived from equation above
-// m = -sum[u]
-// c = getconst(u)
-// have to minimize m*sum[v] + c
-// have to maximize -m*sum[v] - c
-
-/*
-Convex Hull Linear
-needs increasing x/decreasing slopes
-Minimizes Result: mx + b
-Insert lines with slope = m, const = b
-Query best result for x with eval(x)
-Complexity: N [ for infinite partition ], NK [ for K partition ]
-*/
-
-class HullLinear
-{
-	int pointer;////Keeps track of the best line from previous query
-	vector<ll> M; ////Holds the slopes of the lines in the envelope
-	vector<ll> B; ////Holds the y-intercepts of the lines in the envelope
-
-	//Returns true if line l3 is always better than line l2
-	bool bad(int l1, int l2, int l3) {
-		/*
-		intersection(l1,l2) has x-coordinate (b1-b2)/(m2-m1)
-		intersection(l1,l3) has x-coordinate (b1-b3)/(m3-m1)
-		set the former greater than the latter, and cross-multiply to
-		eliminate division. use deterministic comuputation with long
-		long if sufficient.
-		*/
-		return (B[l3] - B[l1]) * (M[l1] - M[l2]) > (B[l2] - B[l1]) * (M[l1] - M[l3]);
+ll Query(ll x) {
+	ll last = M[sz - 1] * x + C[sz - 1];
+	ll l = 0, r = sz - 2;
+	while (l < r) {
+		ll mid = (l + r) / 2;
+		ll cur = M[mid] * x + C[mid];
+		ll after = M[mid + 1] * x + C[mid + 1];
+		if (cur > after)l = mid + 1;
+		else r = mid;
 	}
-public:
-	void Clear() {
-		pointer = 0;
-		M.clear();
-		B.clear();
-	}
-
-	//Adds a new line (with lowest slope) to the structure
-	void Insert(ll m, ll b) {
-		if (M.size() > 0 && M.back() == m)return;///Same Gradient. Don't add.
-		//First, let's add it to the end
-		M.push_back(m);
-		B.push_back(b);
-		//If the penultimate is now made irrelevant between the antepenultimate
-		//and the ultimate, remove it. Repeat as many times as necessary
-		while (M.size() >= 3 && bad(M.size() - 3, M.size() - 2, M.size() - 1)) {
-			M.erase(M.end() - 2);
-			B.erase(B.end() - 2);
-		}
-	}
-
-	//Returns the minimum y-coordinate of any intersection between a given vertical
-	//line and the lower envelope
-	ll Eval(ll x) {
-		// Any better line must be to the right, since query values are
-		//non-decreasing
-		while (pointer < (int) M.size() - 1 && M[pointer + 1]*x + B[pointer + 1] < M[pointer]*x + B[pointer]) {
-			pointer++;
-		}
-		return M[pointer] * x + B[pointer];
-	}
-	ll calcCost(ll p, ll x) {
-		return M[p] * x + B[p];
-	}
-
-	ll onelineQuery(ll x) {
-		ll l = 0, r = M.size() - 1;
-		ll ret = LLONG_MIN;
-		while (abs(l - r) > 3) {
-			ll mid_l = (2 * l + r) / 3;
-			ll mid_r = (l + 2 * r) / 3;
-			if (calcCost(mid_l, x) > calcCost(mid_r, x)) {
-				r = mid_r;
-				ret = max(ret, calcCost(mid_l, x));
-			}
-			else l = mid_l;
-		}
-		for (int i = l; i <= r; i++)
-			ret = max(ret, calcCost(i, x));
-		return ret;
-	}
-} CHT;
-
-int n;
-ll a[N], suff[N], pref[N];
-void solve() {
-	for (int i = 1; i <= n; i++) {
-		suff[i] = suff[i - 1];
-		suff[i] += i * a[i];
-		pref[i] = pref[i - 1] + a[i];
-	}
-	CHT.Clear();
-	ll ans = 0;
-	for (int i = 1; i <= n; i++) {
-		//dbg(i);
-		ans = max(ans, a[i]);
-		ll x = pref[i];
-		if (i > 1) {
-			ll ret = CHT.onelineQuery(x) + suff[i];
-			ans = max(ans, ret);
-		}
-		ll m = -(i - 1);
-		ll b = (i - 1) * pref[i-1] - suff[i - 1];
-		CHT.Insert(m, b);
-	}
-	cout << ans << "\n";
+	return min(last, M[l] * x + C[l]);
 }
-
-
 
 int main()
 {
@@ -223,11 +118,20 @@ int main()
 	freopen("error.txt", "w", stderr);
 #endif
 //*/
+	ll n;
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
+	for (ll i = 1; i <= n; i++) {
+		ll x;
+		cin >> x;
+		S[i] = i * x + S[i - 1];
+		P[i] = x + P[i - 1];
 	}
-	solve();
-	///cout << solve() << "\n";
+	Add(0, 0);
+	ll ans = 0;
+	for (ll i = 1; i <= n; i++) {
+		ans = max(ans, S[i] - Query(P[i]));
+		Add(i, S[i] - i * P[i]);
+	}
+	cout << ans << "\n";
 	return 0;
 }
