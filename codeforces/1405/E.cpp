ebdaf31dbb1406d8060876ee93a8b78668a9c7e1
Mehedi Hasan <hasanmahadi877@gmail.com>
2021-04-15 09:25:00 +0600

    Add solution for problem `E - Fixed Point Removal` Link: https://codeforces.com/contest/1405/problem/E Tags: binary search, data structures, sortings, *2300 Ref: https://codeforces.com/contest/1405/submission/113054918

---
+++ codeforces/1405/E.cpp
@@ -0,0 +1,198 @@
+/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم*/
+
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+typedef long long ll;
+using pii = pair<int, int>;
+const double PI = acos(-1.0);
+const ll mod = 1e9 + 7;
+//const ll mod = 998244353;
+
+
+inline void normal(ll &a) { a %= mod; (a < 0) && (a += mod); }
+inline ll modMul(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a * b) % mod; }
+inline ll modAdd(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); return (a + b) % mod; }
+inline ll modSub(ll a, ll b) { a %= mod, b %= mod; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while (p) { if (p & 1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, mod - 2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+#define si(x) scanf("%d",&x)
+#define sii(x,y) scanf("%d %d",&x,&y)
+#define siii(x,y,z) scanf("%d %d %d",&x,&y,&z)
+#define sl(x) scanf("%lld",&x)
+#define sll(x,y) scanf("%lld %lld",&x,&y)
+#define slll(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define ss(ch) scanf("%s",ch)
+#define pi(x) printf("%d",x)
+#define pii(x,y) printf("%d %d",x,y)
+#define piii(x,y,z) printf("%d %d %d",x,y,z)
+#define pl(x) printf("%lld",x)
+#define pll(x,y) printf("%lld %lld",x,y)
+#define plll(x,y,z) printf("%lld %lld %lld",x,y,z)
+#define ps(ch) printf("%s",ch)
+#define F(i,a,b)      for(int i= a; i <= b; i++)
+#define R(i,b,a)      for(int i= b; i >= a; i--)
+#define REP(i,n) for(int i = 0; i < (n); i++)
+#define sline(a) scanf("%[^\n]s",a)
+#define Case(t) printf("Case %d:\n",t)
+
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1, -1, -1, 0, 1, -1, 0, 1};
+int kx8[] = {1, 1, 2, 2, -1, -1, -2, -2};
+int ky8[] = {2, -2, 1, -1, 2, -2, 1, -1};
+/* for Random Number generate
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+*/
+///**
+template < typename F, typename S >ostream& operator << ( ostream& os, const pair< F, S > & p ) {return os << "(" << p.first << ", " << p.second << ")";}
+template < typename T >ostream &operator << ( ostream & os, const vector< T > &v ) {os << "{"; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << *it;} return os << "}";}
+template < typename T >ostream &operator << ( ostream & os, const set< T > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin()) os << ", "; os << *it;} return os << "]";}
+template < typename F, typename S >ostream &operator << ( ostream & os, const map< F, S > &v ) {os << "["; for (auto it = v.begin(); it != v.end(); ++it) {if ( it != v.begin() ) os << ", "; os << it -> first << " = " << it -> second ;} return os << "]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+clock_t tStart = clock();
+#define timeStamp dbg("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+void faltu () { cerr << endl; }
+template <typename T>void faltu( T a[], int n ) {for (int i = 0; i < n; ++i) cerr << a[i] << ' '; cerr << endl;}
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) { cerr << arg << ' '; faltu(rest...); }
+
+// Program showing a policy-based data structure.
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less 
+using namespace __gnu_pbds;
+
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+// find_by_order(k) – ফাংশনটি kth ordered element এর একটা পয়েন্টার রিটার্ন করে। অর্থাৎ তুমি চাইলেই kth ইন্ডেক্সে কি আছে, সেটা জেনে ফেলতে পারছো!
+// order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
+//*//**___________________________________________________**/
+const int N = 1000006;
+
+
+
+struct BIT
+{
+    int N;
+    vector<int> bit;
+
+    void init(int n)
+    {
+        N = n;
+        bit.assign(n + 1, 0);
+    }
+    //Update always 1 based index e hobe
+    void update(int idx, int val)
+    {
+        while (idx <= N)
+        {
+            bit[idx] += val;
+            idx += idx & -idx;
+        }
+    }
+
+    void updateMax(int idx, int val)
+    {
+        while (idx <= N)
+        {
+            bit[idx] = max(bit[idx], val);
+            idx += idx & -idx;
+        }
+    }
+
+    int pref(int idx)
+    {
+        int ans = 0;
+        while (idx > 0)
+        {
+            ans += bit[idx];
+            idx -= idx & -idx;
+        }
+        return ans;
+    }
+
+    int rsum(int l, int r)
+    {
+        return pref(r) - pref(l - 1);
+    }
+
+    int prefMax(int idx)
+    {
+        int ans = -2e9;
+        while (idx > 0)
+        {
+            ans = max(ans, bit[idx]);
+            idx -= idx & -idx;
+        }
+        return ans;
+    }
+    //Template from Ashishgup
+} fenwick;
+int a[N], ans[N];
+vector<pii> Q[N];
+
+int go(int l, int r, int v) {
+    if (v == 0)return r;
+    for (int i = 20; i >= 0; i--) {
+        int lft = r - (1 << i);
+        if (lft < l)continue;
+        int ck = fenwick.pref(lft);
+        if (ck < v) r -= (1 << i);
+    }
+    return r - 1;
+}
+
+int main()
+{
+    FASTIO
+    ///*
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+//*/
+
+    int n, q;
+    cin >> n >> q;
+    fenwick.init(n + 2);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        a[i] = i - a[i];
+    }
+    for (int i = 1; i <= q; i++) {
+        int x, y;
+        cin >> x >> y;
+        y = n - y;
+        x++;
+        Q[y].push_back({x, i});
+    }
+
+    int rmv = 0;
+    for (int i = 1; i <= n; i++) {
+        if (a[i] >= 0 && a[i] <= rmv) {
+            int mx_id = go(1, i, a[i]);
+            fenwick.update(1, 1);
+            fenwick.update(mx_id + 1, -1);
+            rmv++;
+        }
+        if (Q[i].empty())continue;
+        for (auto &it : Q[i]) {
+            int idx = it.second;
+            int lft = it.first;
+            ans[idx] = fenwick.pref(lft);
+        }
+    }
+    for (int i = 1; i <= q; i++)
+        cout << ans[i] << "\n";
+    return 0;
+}
